79b2fa6ebb49b27d2ed63ec7ed76208d993de478
Patrick Ahrens <pahrens@techfak.uni-bielefeld.de>
2021-03-24 20:01:31 +0100

    creates static lib proj and test proj. Added some rudimentary test code

---
+++ Daxa/include/Daxa.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+
+#include "../src/DaxaCore.hpp"
+
+#include "../dependencies/stb_image.hpp"
+#include "../dependencies/tiny_obj_loader.hpp"
---
+++ DaxaTriangle/DaxaTriangle.cpp
@@ -0,0 +1,47 @@
+#include "Daxa.hpp"
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0xFF));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Wait two seconds
+            SDL_Delay(2000);
+        }
+    }
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
