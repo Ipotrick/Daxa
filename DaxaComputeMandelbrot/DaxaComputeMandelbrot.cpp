2a4d2c64c14a416781579a1482da175abb0946d1
Ipotrick <patrick.ahrens.32369@web.de>
2021-12-15 22:10:31 +0100

    finished compute example

--- DaxaComputeMandelbrot/DaxaComputeMandelbrot.cpp
+++ DaxaComputeMandelbrot/DaxaComputeMandelbrot.cpp
@@ -11,6 +11,7 @@ public:
 			.surface = app.window->getSurface(), 
 			.width = app.window->getSize()[0], 
 			.height = app.window->getSize()[1], 
+			.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR,
 			.additionalUses = VK_IMAGE_USAGE_TRANSFER_DST_BIT,
 		})}
 		, swapchainImage{ this->swapchain->aquireNextImage() }
@@ -23,18 +24,52 @@ public:
 			layout(local_size_x = 8, local_size_y = 8) in;
 
 			layout(set = 0, binding = 0) uniform CameraData {
-				uint imageWidth;
-				uint imageHeight;
+				ivec2 imgSize;
 			} cameraData;
 
 			layout (set = 0, binding = 1, rgba8) uniform writeonly image2D resultImage;
 
+			float mandelbrot(in vec2 c) {const float B = 256.0;
+				float l = 0.0;
+				vec2 z  = vec2(0.0);
+				for( int i=0; i<1024; i++ )
+				{
+					z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + c;
+					if( dot(z,z)>(B*B) ) break;
+					l += 1.0;
+				}
+
+				if( l>511.0 ) return 0.0;
+
+				float sl = l - log2(log2(dot(z,z))) + 4.0;
+
+				l = mix( l, sl, 1.0f );
+
+				return l;
+			}
+
 			void main()
 			{
-				if (gl_GlobalInvocationID.x < cameraData.imageWidth && gl_GlobalInvocationID.y < cameraData.imageHeight) {
+				if (gl_GlobalInvocationID.x < cameraData.imgSize.x && gl_GlobalInvocationID.y < cameraData.imgSize.y) {
+					vec2 smoothCoord = vec2(gl_GlobalInvocationID.xy) / vec2(cameraData.imgSize.xy) * 2.0f - vec2(1,1);
+					smoothCoord *= 2.0f;
+
+					vec4 colorAcc = vec4(0,0,0,0);
+
+					const int AA = 4;
+					vec2 aaStep = vec2(1,1) / vec2(cameraData.imgSize.xy) / AA * 4;
+
+					for (int x = 0; x < AA; x++) {
+						for (int y = 0; y < AA; y++) {
+							vec2 subCoord = smoothCoord + aaStep * vec2(x,y);
+
+							colorAcc += vec4(1.0f,0.8f,0.8f,1.0f) * mandelbrot(subCoord)*(1.0f/80.0f);
+						}
+					}
+					colorAcc *= 1.0f / (AA*AA);
+
 					ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
-					vec4 color = vec4(float(gl_GlobalInvocationID.x) / float(cameraData.imageWidth), float(gl_GlobalInvocationID.y) / float(cameraData.imageHeight), 1.0f, 1.0f);
-					imageStore(resultImage, coord, color);
+					imageStore(resultImage, coord, colorAcc);
 				}
 			}
 		)";
@@ -193,7 +228,7 @@ int main()
 	daxa::initialize();
 
 	{
-		daxa::Application<MyUser> app{ 1000, 1000, "Daxa Compute Sample"};
+		daxa::Application<MyUser> app{ 640, 640, "Daxa Compute Sample"};
 		app.run();
 	}
 
