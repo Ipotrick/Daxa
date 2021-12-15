ac29abea79644d8ab8866b9c335ee0a22c62c779
Ipotrick <patrick.ahrens.32369@web.de>
2021-12-15 04:24:07 +0100

    Bugfixes in Image class and Image upload functions.

--- Daxa/src/gpu/CommandList.cpp
+++ Daxa/src/gpu/CommandList.cpp
@@ -150,6 +150,7 @@ namespace daxa {
 				.waitingAccess = VK_ACCESS_2_MEMORY_READ_BIT_KHR,
 				.layoutBefore = VK_IMAGE_LAYOUT_UNDEFINED,
 				.layoutAfter = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				.image = copyInfo.dst,
 			};
 			insertBarriers({},{},{&firstBarrier, 1});
 
@@ -161,8 +162,9 @@ namespace daxa {
 				.waitingStages = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT_KHR,
 				.layoutBefore = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
 				.layoutAfter = finalImageLayout,
+				.image = copyInfo.dst,
 			};
-			insertBarriers({},{},{&firstBarrier, 1});
+			insertBarriers({},{},{&secondBarrier, 1});
 		}
 
 		void CommandList::copyMultiBufferToBuffer(BufferToBufferMultiCopyInfo copyInfo) {
