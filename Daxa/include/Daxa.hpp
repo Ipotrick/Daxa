0e7085e45eb13778128ba7ffe78e9f130e7cc6f3
Ipotrick <patrick.ahrens.32369@web.de>
2022-01-02 00:49:33 +0100

    added new project, started work on meshes/models

--- Daxa/include/Daxa.hpp
+++ Daxa/include/Daxa.hpp
@@ -4,6 +4,8 @@
 #include "../src/gpu/Device.hpp"
 #include "../src/gpu/Instance.hpp"
 #include "../src/DearImGuiImpl.hpp"
+#include "../src/rendering/Cameras.hpp"
+#include "../src/rendering/Mesh.hpp"
 
 namespace daxa {
 	void initialize();
