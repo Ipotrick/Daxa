bc5816099b2191ced8983de81ed3e04069365d5c
GabeRundlett <gabe@rundlett.com>
2022-03-16 18:37:00 -0600

    Fix CMake Presets and DaxaMinecraft

--- DaxaMinecraft/src/engine/window/sdl2/input.hpp
+++ DaxaMinecraft/src/engine/window/sdl2/input.hpp
@@ -1,7 +1,17 @@
 #pragma once
+
+#define USING_GLFW
+// #define USING_SDL2
+
+#if defined(USING_SDL2)
 #include <SDL2/SDL_keycode.h>
+#elif defined(USING_GLFW)
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#endif
 
 namespace input::keybinds {
+#if defined(USING_SDL2)
     static constexpr auto MOVE_PZ = SDLK_w;
     static constexpr auto MOVE_NZ = SDLK_s;
     static constexpr auto MOVE_PX = SDLK_a;
@@ -11,4 +21,15 @@ namespace input::keybinds {
     static constexpr auto MOVE_NY = SDLK_LSHIFT;
 
     static constexpr auto TOGGLE_PAUSE = SDLK_ESCAPE;
+#elif defined(USING_GLFW)
+    static constexpr auto MOVE_PZ = GLFW_KEY_W;
+    static constexpr auto MOVE_NZ = GLFW_KEY_S;
+    static constexpr auto MOVE_PX = GLFW_KEY_A;
+    static constexpr auto MOVE_NX = GLFW_KEY_D;
+
+    static constexpr auto MOVE_PY = GLFW_KEY_SPACE;
+    static constexpr auto MOVE_NY = GLFW_KEY_LEFT_SHIFT;
+
+    static constexpr auto TOGGLE_PAUSE = GLFW_KEY_ESCAPE;
+#endif
 } // namespace input::keybinds
