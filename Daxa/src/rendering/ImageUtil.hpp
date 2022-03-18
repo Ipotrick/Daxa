4a4e2ba53f8fb6a70322dbc8e12ec33a95853445
Ipotrick <patrick.ahrens.32369@web.de>
2022-03-19 00:26:46 +0100

    split out image generation, ixed bug with multi includes violating definition of one

---
+++ Daxa/src/rendering/ImageUtil.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../DaxaCore.hpp"
+
+#include <unordered_map>
+#include <filesystem>
+#include <fstream>
+
+#include "stb_image.h"
+
+#include "../gpu/Device.hpp"
+
+namespace daxa {
+    /**
+     * @brief generated mipmaps for a 2d texture for layer 0. 
+     * The incoming image layout at level 0 must be VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL.
+     * All image level layouts will be set to dstImageLayoutsAllLevels in the function.
+     * 
+     * @param cmdList command list the mip generation commands are recorded to.
+     * @param img to generate mipmaps  for. The layout of level 0 must be VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL.
+     * @param dstImageLayoutsAllLevels the layout of all image levels after the mip generation.
+     */
+    void generateMipLevels(gpu::CommandListHandle& cmdList, gpu::ImageViewHandle& img, VkImageLayout dstImageLayoutsAllLevels);
+}
