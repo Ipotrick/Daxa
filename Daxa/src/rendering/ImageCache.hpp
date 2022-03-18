9bc606d2f246655dfb2948982651c0f7f9eeb91a
Ipotrick <patrick.ahrens.32369@web.de>
2022-03-19 00:02:48 +0100

    added trilinear mippmapped anisotropic filtering to images in imagecache

--- Daxa/src/rendering/ImageCache.hpp
+++ Daxa/src/rendering/ImageCache.hpp
@@ -76,6 +76,129 @@ namespace daxa {
 
         std::unordered_map<ImageCacheFetchInfo, gpu::ImageViewHandle, ImageCacheFetchInfoHasher> cache = {};
     private:
+        void generateMipLevels(gpu::CommandListHandle& cmdList, gpu::ImageViewHandle& img) {
+            i32 mipwidth = img->getImageHandle()->getVkExtent3D().width;
+            i32 mipheight = img->getImageHandle()->getVkExtent3D().height;
+            for (u32 i = 0; i < img->getImageHandle()->getMipLevels() - 1; i++) {
+                cmdList->insertImageBarriers(std::array{
+                    gpu::ImageBarrier{
+                        .barrier = {
+                            .srcStages = VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
+                            .srcAccess = VK_ACCESS_2_TRANSFER_WRITE_BIT_KHR,
+                            .dstStages = VK_PIPELINE_STAGE_2_BLIT_BIT_KHR,
+                            .dstAccess = VK_ACCESS_2_TRANSFER_READ_BIT_KHR,
+                        },
+                        .image = img,
+                        .layoutBefore = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                        .layoutAfter = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+                        .subRange = {VkImageSubresourceRange{
+                            .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                            .baseMipLevel = i,
+                            .levelCount = 1,
+                            .baseArrayLayer = 0,
+                            .layerCount = 1,
+                        }}
+                    },
+                    gpu::ImageBarrier{
+                        .barrier = {
+                            .dstStages = VK_PIPELINE_STAGE_2_BLIT_BIT_KHR,
+                            .dstAccess = VK_ACCESS_2_TRANSFER_READ_BIT_KHR,
+                        },
+                        .image = img,
+                        .layoutAfter = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                        .subRange = {VkImageSubresourceRange{
+                            .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                            .baseMipLevel = i+1,
+                            .levelCount = 1,
+                            .baseArrayLayer = 0,
+                            .layerCount = 1,
+                        }}
+                    },
+                });
+                VkImageBlit blit{
+                    .srcOffsets = {
+                        VkOffset3D{ 0, 0, 0 },
+                        VkOffset3D{ mipwidth, mipheight, 1},
+                    },
+                    .srcSubresource = VkImageSubresourceLayers{
+                        .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                        .mipLevel = i,
+                        .baseArrayLayer = 0,
+                        .layerCount = 1,
+                    },
+                    .dstOffsets = {
+                        VkOffset3D{ 0, 0, 0 },
+                        VkOffset3D{ std::max(1, mipwidth / 2), std::max(1, mipheight / 2), 1},
+                    },
+                    .dstSubresource = VkImageSubresourceLayers{
+                        .aspectMask = VkImageAspectFlagBits::VK_IMAGE_ASPECT_COLOR_BIT,
+                        .mipLevel = i+1,
+                        .baseArrayLayer = 0,
+                        .layerCount = 1,
+                    },
+                };
+
+                vkCmdBlitImage(
+                    cmdList->getVkCommandBuffer(), 
+                    img->getImageHandle()->getVkImage(),
+                    VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+                    img->getImageHandle()->getVkImage(),
+                    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                    1,
+                    &blit,
+                    VK_FILTER_LINEAR
+                );
+
+                mipwidth = std::max(1, mipwidth / 2);
+                mipheight = std::max(1, mipheight / 2);
+            }
+
+            std::array<gpu::ImageBarrier, 32> buff;
+            size_t buffSize = 0;
+
+            for (u32 i = 0; i < img->getImageHandle()->getMipLevels() - 1; i++) {
+                buff[buffSize++] = gpu::ImageBarrier{
+                    .barrier = gpu::MemoryBarrier{
+                        .srcStages = VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
+                        .srcAccess = VK_ACCESS_2_MEMORY_READ_BIT_KHR | VK_ACCESS_2_MEMORY_WRITE_BIT_KHR,
+                        .dstStages = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT_KHR,
+                        .dstAccess = VK_ACCESS_2_MEMORY_READ_BIT_KHR | VK_ACCESS_2_MEMORY_WRITE_BIT_KHR,
+                    },
+                    .image = img,
+                    .layoutBefore = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+                    .layoutAfter = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                    .subRange = {VkImageSubresourceRange{
+                        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                        .baseMipLevel = i,
+                        .levelCount = 1,
+                        .baseArrayLayer = 0,
+                        .layerCount = 1,
+                    }},
+                };
+            }
+
+            buff[buffSize++] = gpu::ImageBarrier{
+                .barrier = {
+                    .srcStages = VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
+                    .srcAccess = VK_ACCESS_2_MEMORY_READ_BIT_KHR | VK_ACCESS_2_MEMORY_WRITE_BIT_KHR,
+                    .dstStages = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT_KHR,
+                    .dstAccess = VK_ACCESS_2_MEMORY_READ_BIT_KHR | VK_ACCESS_2_MEMORY_WRITE_BIT_KHR,
+                },
+                .image = img,
+                .layoutBefore = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                .layoutAfter = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                .subRange = {VkImageSubresourceRange{
+                    .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                    .baseMipLevel = img->getImageHandle()->getMipLevels() - 1,
+                    .levelCount = 1,
+                    .baseArrayLayer = 0,
+                    .layerCount = 1,
+                }},
+            };
+
+            cmdList->insertBarriers({}, {buff.data(), buffSize});
+        }
+
         gpu::ImageViewHandle loadImage(ImageCacheFetchInfo const& info, gpu::CommandListHandle& cmdList) {
             //stbi_set_flip_vertically_on_load(1);
             int width, height, channels;
@@ -87,6 +210,8 @@ namespace daxa {
                 data = stbi_load((char const*)info.path.string().c_str(), &width, &height, &channels, 4);
             }
             printf("loaded image from path: %s\n", (char const*)info.path.string().c_str());
+            
+            u32 mipmaplevels = std::log2(std::max(width, height));
 
             if (!data) {
                 return {};
@@ -95,20 +220,55 @@ namespace daxa {
                     .image = device->createImage({
                         .format = info.viewFormat,
                         .extent = { (u32)width, (u32)height, 1 },
-                        .usage = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT
+                        .usage = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+                        .mipLevels = mipmaplevels,
                     }),
                     .format = info.viewFormat, 
                 };
                 if (info.samplerInfo.has_value()) {
                     printf("create with sampler\n");
-                    auto& sampler_v = info.samplerInfo.value();
+                    auto sampler_v = info.samplerInfo.value();
+                    sampler_v.anisotropyEnable = VK_TRUE;
+                    sampler_v.maxAnisotropy = 16.0f;
+                    sampler_v.mipmapMode = VkSamplerMipmapMode::VK_SAMPLER_MIPMAP_MODE_LINEAR;
                     if (!samplers.contains(sampler_v)) {
                         samplers[sampler_v] = device->createSampler(sampler_v);
                     }
                     ci.defaultSampler = samplers[sampler_v];
+                    ci.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+                    ci.subresourceRange.baseMipLevel = 0;
+                    ci.subresourceRange.levelCount = mipmaplevels;
+                    ci.subresourceRange.baseArrayLayer = 0;
+                    ci.subresourceRange.layerCount = 1;
                 }
                 auto image = device->createImageView(ci);
-                cmdList->copyHostToImageSynced({ .src = data, .dst = image, .size = (u32)(width*height*4), .dstFinalLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL });
+
+                cmdList->insertImageBarrier({
+                    .layoutAfter = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                    .image = image,
+                });
+                cmdList->copyHostToImage({
+                    .src = data,
+                    .dst = image,
+                    .size = static_cast<u32>(width*height*4),
+                });
+                if (false) {
+                    cmdList->insertImageBarrier({
+                        .barrier = {
+                            .srcStages = gpu::STAGE_TRANSFER,
+                            .srcAccess = gpu::ACCESS_MEMORY_WRITE,
+                            .dstStages = gpu::STAGE_ALL_COMMANDS,
+                            .srcAccess = gpu::ACCESS_MEMORY_READ,
+                        },
+                        .layoutBefore = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                        .layoutAfter = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                        .image = image,
+                    });
+                }
+                else {
+                    generateMipLevels(cmdList, image);
+                }
+
                 std::free(data);
                 return image;
             }
