448754b3a4c8d3644d58f7dc9de5ca9a8582eb53
Ipotrick <patrick.ahrens.32369@web.de>
2022-03-19 21:34:42 +0100

    simplified ressource handling of descriptor indices. Added dynamic array texture indexing feature.

--- Daxa/src/gpu/Image.hpp
+++ Daxa/src/gpu/Image.hpp
@@ -111,26 +111,8 @@ namespace daxa {
 			VkImageSubresourceRange getVkImageSubresourceRange() const { return subresourceRange; }
 			VkImageView getVkImageView() const { return view; }
 			SamplerHandle const& getSampler() const { return defaultSampler; }
-			std::optional<u16> getSampledImageDescriptorIndex() const { 
-				if (sampledImageIndex == 0) {
-					return std::nullopt;
-				} else {
-					return sampledImageIndex;
-				}
-			}
-			std::optional<u16> getCombinedImageSamplerDescriptorIndex() const { 
-				if (imageSamplerIndex == 0) {
-					return std::nullopt;
-				} else {
-					return imageSamplerIndex;
-				} 
-			}
-			std::optional<u16> getStorageImageDescriptorIndex() const { 
-				if (storageImageIndex == 0) {
-					return std::nullopt;
-				} else {
-					return storageImageIndex;
-				} 
+			u16 getDescriptorIndex() const {
+				return descriptorIndex;
 			}
 			std::string const& getDebugName() const { return debugName; }
 		private:
@@ -147,9 +129,7 @@ namespace daxa {
 			VkImageSubresourceRange    		subresourceRange	= {};
 			VkImageView						view				= {};
 			SamplerHandle 					defaultSampler 		= {};
-			u16 							sampledImageIndex 	= {};
-			u16 							imageSamplerIndex 	= {};
-			u16 							storageImageIndex 	= {};
+			u16 							descriptorIndex 	= {};
 			std::string 					debugName 			= {};
 		};
 
