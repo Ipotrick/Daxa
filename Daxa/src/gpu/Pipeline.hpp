42282fff3e0b2c678b3e890c2bc6ccd22f92fcc5
Ipotrick <patrick.ahrens.32369@web.de>
2022-03-23 02:09:49 +0100

    added pipeline hot reloading utility functions to pipeline compiler

--- Daxa/src/gpu/Pipeline.hpp
+++ Daxa/src/gpu/Pipeline.hpp
@@ -2,6 +2,7 @@
 
 #include "../DaxaCore.hpp"
 
+#include <set>
 #include <memory>
 #include <unordered_map>
 #include <map>
@@ -64,13 +65,91 @@ namespace daxa {
 
 		constexpr inline size_t MAX_SETS_PER_PIPELINE = 4;
 
+		struct DepthTestSettings {
+			VkFormat 	depthAttachmentFormat 	= VK_FORMAT_UNDEFINED;
+			bool 		enableDepthTest 		= false;
+			bool 		enableDepthWrite 		= false;
+			VkCompareOp depthTestCompareOp 		= VK_COMPARE_OP_LESS_OR_EQUAL;
+			f32 		minDepthBounds 			= 0.0f;
+			f32 		maxDepthBounds 			= 1.0f;
+		};
+
+		struct RasterSettings {
+			VkPolygonMode 		polygonMode 			= VK_POLYGON_MODE_FILL;
+			VkCullModeFlagBits 	cullMode 				= VK_CULL_MODE_NONE;
+			VkFrontFace 		frontFace 				= VK_FRONT_FACE_CLOCKWISE;
+			bool 				depthClampEnable 		= false;
+			bool 				rasterizerDiscardEnable = false;
+			bool 				depthBiasEnable 		= false;
+			float 				depthBiasConstantFactor = 0.0f;
+			float 				depthBiasClamp 			= 0.0f;
+			float 				depthBiasSlopeFactor 	= 0.0f;
+			f32 				lineWidth 				= 1.0f;
+		};
+
+		std::map<uint32_t, std::map<uint32_t, VkDescriptorSetLayoutBinding>>
+		reflectSetBindings(const std::vector<uint32_t>& spv, VkShaderStageFlagBits shaderStage);
+
+		void reflectShader(
+			ShaderModuleHandle const& shaderModule, 
+			std::vector<VkPushConstantRange>& pushConstants, 
+			std::vector<BindingSetDescription>& bindingSetDescriptions
+		);
+
+		std::vector<VkDescriptorSetLayout> processReflectedDescriptorData(
+			std::vector<BindingSetDescription>& setDescriptions,
+			BindingSetLayoutCache& descCache,
+			std::array<std::shared_ptr<BindingSetLayout const>, MAX_SETS_PER_PIPELINE>& setLayouts
+		);
+
+		std::vector<VkPushConstantRange> reflectPushConstants(const std::vector<uint32_t>& spv, VkShaderStageFlagBits shaderStage);
+
 		struct ComputePipelineCreateInfo {
 			ShaderModuleCreateInfo 													shaderCI 	= {};
 			std::array<std::optional<BindingSetDescription>, MAX_SETS_PER_PIPELINE> overwriteSets 	= {};
 			char const* 															debugName 		= {};
 		};
 
-		class PipelineHandle;
+		class GraphicsPipelineBuilder {
+		public:
+			GraphicsPipelineBuilder() = default;
+			GraphicsPipelineBuilder& setDebugName(char const* name);
+			GraphicsPipelineBuilder& beginVertexInputAttributeBinding(VkVertexInputRate rate);
+			GraphicsPipelineBuilder& addVertexInputAttribute(VkFormat format);
+			GraphicsPipelineBuilder& addVertexInputAttributeSpacer(u32 spacing);
+			GraphicsPipelineBuilder& endVertexInputAttributeBinding();
+			GraphicsPipelineBuilder& configurateDepthTest(DepthTestSettings const& depthTestSettings2);
+			GraphicsPipelineBuilder& setInputAssembly(VkPipelineInputAssemblyStateCreateInfo const& inputassembly);
+			GraphicsPipelineBuilder& setRasterization(RasterSettings const& rasterization);
+			GraphicsPipelineBuilder& setMultisampling(VkPipelineMultisampleStateCreateInfo const& multisampling);
+			GraphicsPipelineBuilder& addShaderStage(ShaderModuleCreateInfo const& ci);
+			GraphicsPipelineBuilder& addColorAttachment(VkFormat const& attachmentFormat);
+			GraphicsPipelineBuilder& addColorAttachment(VkFormat const& attachmentFormat, const VkPipelineColorBlendAttachmentState&);
+			GraphicsPipelineBuilder& overwriteSet(u32 set, BindingSetDescription const& descr);
+		private:
+			friend class Device;
+			friend class daxa::PipelineCompiler;
+
+			std::string debugName = {};
+
+			// Vertex Input Attribute building:
+			bool bVertexAtrributeBindingBuildingOpen = false;
+			VkVertexInputRate currentVertexAttributeBindingInputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+			u32 currentVertexAttributeBindingOffset = 0;
+			u32 currentVertexAttributeLocation = 0;
+			std::vector<VkVertexInputBindingDescription> vertexInputBindingDescriptions;
+			std::vector<VkVertexInputAttributeDescription> vertexInputAttributeDescriptions;
+
+			DepthTestSettings depthTestSettings = {};
+			RasterSettings rasterSettings = {};
+			std::optional<VkPipelineInputAssemblyStateCreateInfo> inputAssembly;
+			std::optional<VkPipelineMultisampleStateCreateInfo> multisampling;
+			std::vector<VkDynamicState> dynamicStateEnable;
+			std::vector<ShaderModuleCreateInfo> shaderModuleCIs;
+			std::vector<VkPipelineColorBlendAttachmentState> colorAttachmentBlends;
+			std::vector<VkFormat> colorAttachmentFormats;
+			std::vector<std::pair<u32, BindingSetDescription>> setDescriptionOverwrites;
+		};
 
 		std::size_t sizeofFormat(VkFormat format);
 
@@ -104,101 +183,21 @@ namespace daxa {
 			friend daxa::Result<PipelineHandle> createComputePipeline(std::shared_ptr<DeviceBackend>& deviceBackend, BindingSetLayoutCache& bindingSetCache, ComputePipelineCreateInfo const& ci);
 			friend void setPipelineDebugName(VkDevice device, char const* debugName, Pipeline& pipeline);
 
-			std::shared_ptr<DeviceBackend>												deviceBackend			= {};
-			std::array<std::shared_ptr<BindingSetLayout const>, MAX_SETS_PER_PIPELINE> 	bindingSetLayouts 	= {};
-			std::vector<VkFormat> 														colorAttachmentFormats 	= {};
-			VkFormat 																	depthAttachment 		= VK_FORMAT_UNDEFINED;
-			VkFormat 																	stencilAttachment 		= VK_FORMAT_UNDEFINED;
-			VkPipelineBindPoint 														bindPoint 				= {};
-			VkPipeline 																	pipeline				= {};
-			VkPipelineLayout 															layout					= {};
-			std::string 																debugName 				= {};
+			std::shared_ptr<DeviceBackend>														deviceBackend			= {};
+			std::array<std::shared_ptr<BindingSetLayout const>, MAX_SETS_PER_PIPELINE> 			bindingSetLayouts 		= {};
+			std::vector<VkFormat> 																colorAttachmentFormats 	= {};
+			VkFormat 																			depthAttachment 		= VK_FORMAT_UNDEFINED;
+			VkFormat 																			stencilAttachment 		= VK_FORMAT_UNDEFINED;
+			VkPipelineBindPoint 																bindPoint 				= {};
+			VkPipeline 																			pipeline				= {};
+			VkPipelineLayout 																	layout					= {};
+			std::string 																		debugName 				= {};
+			std::variant<GraphicsPipelineBuilder, ComputePipelineCreateInfo, std::monostate> 	creator 				= {};
+			std::set<std::pair<std::filesystem::path, std::chrono::file_clock::time_point>>		observedHotLoadFiles	= {};
 		};
 
 		class PipelineHandle : public SharedHandle<Pipeline>{};
 
-		struct DepthTestSettings {
-			VkFormat 	depthAttachmentFormat 	= VK_FORMAT_UNDEFINED;
-			bool 		enableDepthTest 		= false;
-			bool 		enableDepthWrite 		= false;
-			VkCompareOp depthTestCompareOp 		= VK_COMPARE_OP_LESS_OR_EQUAL;
-			f32 		minDepthBounds 			= 0.0f;
-			f32 		maxDepthBounds 			= 1.0f;
-		};
-
-		struct RasterSettings {
-			VkPolygonMode 		polygonMode 			= VK_POLYGON_MODE_FILL;
-			VkCullModeFlagBits 	cullMode 				= VK_CULL_MODE_NONE;
-			VkFrontFace 		frontFace 				= VK_FRONT_FACE_CLOCKWISE;
-			bool 				depthClampEnable 		= false;
-			bool 				rasterizerDiscardEnable = false;
-			bool 				depthBiasEnable 		= false;
-			float 				depthBiasConstantFactor = 0.0f;
-			float 				depthBiasClamp 			= 0.0f;
-			float 				depthBiasSlopeFactor 	= 0.0f;
-			f32 				lineWidth 				= 1.0f;
-		};
-
-		class GraphicsPipelineBuilder {
-		public:
-			GraphicsPipelineBuilder() = default;
-			GraphicsPipelineBuilder& setDebugName(char const* name);
-			GraphicsPipelineBuilder& beginVertexInputAttributeBinding(VkVertexInputRate rate);
-			GraphicsPipelineBuilder& addVertexInputAttribute(VkFormat format);
-			GraphicsPipelineBuilder& addVertexInputAttributeSpacer(u32 spacing);
-			GraphicsPipelineBuilder& endVertexInputAttributeBinding();
-			GraphicsPipelineBuilder& configurateDepthTest(DepthTestSettings const& depthTestSettings2);
-			GraphicsPipelineBuilder& setInputAssembly(VkPipelineInputAssemblyStateCreateInfo const& inputassembly);
-			GraphicsPipelineBuilder& setRasterization(RasterSettings const& rasterization);
-			GraphicsPipelineBuilder& setMultisampling(VkPipelineMultisampleStateCreateInfo const& multisampling);
-			//GraphicsPipelineBuilder& addShaderStage(ShaderModuleHandle const& shaderModule);
-			GraphicsPipelineBuilder& addShaderStage(ShaderModuleCreateInfo const& ci);
-			GraphicsPipelineBuilder& addColorAttachment(VkFormat const& attachmentFormat);
-			GraphicsPipelineBuilder& addColorAttachment(VkFormat const& attachmentFormat, const VkPipelineColorBlendAttachmentState&);
-			GraphicsPipelineBuilder& overwriteSet(u32 set, BindingSetDescription const& descr);
-		private:
-			friend class Device;
-			friend class daxa::PipelineCompiler;
-
-			std::string debugName = {};
-
-			// Vertex Input Attribute building:
-			bool bVertexAtrributeBindingBuildingOpen = false;
-			VkVertexInputRate currentVertexAttributeBindingInputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-			u32 currentVertexAttributeBindingOffset = 0;
-			u32 currentVertexAttributeLocation = 0;
-			std::vector<VkVertexInputBindingDescription> vertexInputBindingDescriptions;
-			std::vector<VkVertexInputAttributeDescription> vertexInputAttributeDescriptions;
-
-			VkPipelineCache pipelineCache;
-			DepthTestSettings depthTestSettings = {};
-			RasterSettings rasterSettings = {};
-			std::optional<VkPipelineInputAssemblyStateCreateInfo> inputAssembly;
-			std::optional<VkPipelineMultisampleStateCreateInfo> multisampling;
-			std::vector<VkDynamicState> dynamicStateEnable;
-			std::vector<ShaderModuleCreateInfo> shaderModuleCIs;
-			std::vector<VkPipelineColorBlendAttachmentState> colorAttachmentBlends;
-			std::vector<VkFormat> colorAttachmentFormats;
-			std::vector<std::pair<u32, BindingSetDescription>> setDescriptionOverwrites;
-		};
-
 		void setPipelineDebugName(VkDevice device, char const* debugName, Pipeline& pipeline);
-
-		std::map<uint32_t, std::map<uint32_t, VkDescriptorSetLayoutBinding>>
-		reflectSetBindings(const std::vector<uint32_t>& spv, VkShaderStageFlagBits shaderStage);
-
-		void reflectShader(
-			ShaderModuleHandle const& shaderModule, 
-			std::vector<VkPushConstantRange>& pushConstants, 
-			std::vector<BindingSetDescription>& bindingSetDescriptions
-		);
-
-		std::vector<VkDescriptorSetLayout> processReflectedDescriptorData(
-			std::vector<BindingSetDescription>& setDescriptions,
-			BindingSetLayoutCache& descCache,
-			std::array<std::shared_ptr<BindingSetLayout const>, MAX_SETS_PER_PIPELINE>& setLayouts
-		);
-
-		std::vector<VkPushConstantRange> reflectPushConstants(const std::vector<uint32_t>& spv, VkShaderStageFlagBits shaderStage);
 	}
 }
