a0bc509b0e06410a8432a89eb4a478086505eb5b
GabeRundlett <gabe@rundlett.com>
2022-03-27 14:19:52 -0600

    move indexs from Storage globals instead of Push constants

--- DaxaMinecraft/assets/shaders/utils/common.glsl
+++ DaxaMinecraft/assets/shaders/utils/common.glsl
@@ -6,11 +6,11 @@ layout(set = 0, binding = 4) buffer Globals {
     ivec2 frame_dim;
     float time;
 
-    // uint chunk_image_0i, chunk_image_1i;
+    uint chunk_image_0i, chunk_image_1i;
 }
 globals_sb_view[];
 
 #define globals globals_sb_view[p.globals_sb]
-#define chunk_image_0 input_images[p.chunk_image_0i]
-#define chunk_image_1 input_images[p.chunk_image_1i]
+#define chunk_image_0 input_images[globals.chunk_image_0i]
+#define chunk_image_1 input_images[globals.chunk_image_1i]
 #define output_image output_images[p.output_image_i]
