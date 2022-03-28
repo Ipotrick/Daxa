3f251f7dc954e1d4c3ba69574cc7f10fd28ece17
GabeRundlett <gabe@rundlett.com>
2022-03-27 20:02:38 -0600

    Refactoring shaders update

--- DaxaMinecraft/assets/shaders/chunkgen/common.glsl
+++ DaxaMinecraft/assets/shaders/chunkgen/common.glsl
@@ -3,6 +3,7 @@ layout(push_constant) uniform Push {
     vec4 pos;
     uint globals_sb;
     uint output_image_i;
+    uint chunk_buffer_i;
 } p;
 
 layout(set = 0, binding = 3, r32ui) uniform readonly uimage3D input_images[];
