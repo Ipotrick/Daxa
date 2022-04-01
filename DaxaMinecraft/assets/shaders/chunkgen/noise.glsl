a55d0d19338719da15c5df41fbbf39135a5d7c86
GabeRundlett <gabe@rundlett.com>
2022-03-31 23:30:21 -0600

    ADD SDF TO DAXA MINECRAFT!

--- DaxaMinecraft/assets/shaders/chunkgen/noise.glsl
+++ DaxaMinecraft/assets/shaders/chunkgen/noise.glsl
@@ -3,21 +3,32 @@
 float terrain_noise(vec3 pos) {
     FractalNoiseConfig noise_conf = {
         /* .amplitude   = */ 0.1f,
-        /* .persistance = */ 0.6f,
-        /* .scale       = */ 0.005f,
+        /* .persistance = */ 0.5f,
+        /* .scale       = */ 0.002f,
         /* .lacunarity  = */ 2,
-        /* .octaves     = */ 4,
+        /* .octaves     = */ 6,
     };
-    return fractal_noise(pos, noise_conf) * 3.0f + pos.y * 0.005 - 0.2;
+    return fractal_noise(pos, noise_conf) * 10.0f + pos.y * 0.01 - 0.8;
 }
 
 float biome_noise(vec3 pos) {
     FractalNoiseConfig noise_conf = {
         /* .amplitude   = */ 0.1f,
         /* .persistance = */ 0.4f,
-        /* .scale       = */ 0.005f,
+        /* .scale       = */ 0.001f,
         /* .lacunarity  = */ 4,
         /* .octaves     = */ 4,
     };
     return fractal_noise(vec3(pos.x, 0, pos.z) + 1000, noise_conf) * 3.0f;
+}
+
+float underworld_noise(vec3 pos) {
+    FractalNoiseConfig noise_conf = {
+        /* .amplitude   = */ 0.1f,
+        /* .persistance = */ 0.5f,
+        /* .scale       = */ 0.002f,
+        /* .lacunarity  = */ 2,
+        /* .octaves     = */ 6,
+    };
+    return fractal_noise(pos - vec3(0, 0, -1000), noise_conf) * 10.0f + abs(500 - pos.y) * 0.006 - 0.2;
 }
--- DaxaMinecraft/assets/shaders/utils/common.glsl
+++ DaxaMinecraft/assets/shaders/utils/common.glsl
@@ -1,9 +1,10 @@
 #extension GL_EXT_nonuniform_qualifier : enable
 
-const uvec3 CHUNK_N = uvec3(8, 2, 8);
+const uvec3 CHUNK_N = uvec3(12, 12, 12);
 const uvec3 CHUNK_SIZE = uvec3(64);
 
 const uint MAX_STEPS = CHUNK_N.x * CHUNK_SIZE.x + CHUNK_N.y * CHUNK_SIZE.y + CHUNK_N.z * CHUNK_SIZE.z;
+const uint WATER_LEVEL = 160;
 
 layout(set = 0, binding = 4) buffer Globals {
     mat4 viewproj_mat;
@@ -25,7 +26,7 @@ uint get_tile(vec3 pos) {
     if (chunk_i.x < 0 || chunk_i.x > CHUNK_N.x - 1 ||
         chunk_i.y < 0 || chunk_i.y > CHUNK_N.y - 1 ||
         chunk_i.z < 0 || chunk_i.z > CHUNK_N.z - 1) {
-        return 0;
+        return 0xff000000;
     }
     return imageLoad(chunk_images(chunk_i), ivec3(pos.x, pos.y, pos.z) - chunk_i * ivec3(CHUNK_SIZE)).r;
 }
@@ -35,7 +36,8 @@ uint get_block_id(vec3 p) {
 }
 
 bool is_voxel_occluding(vec3 p) {
-    return get_block_id(p) != 0;
+    uint id = get_block_id(p);
+    return id != 0;
 }
 
 bool is_tile_occluding(uint tile) {
