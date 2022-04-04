c69c60577cff8cbf95e145ab06668c566f8f28eb
GabeRundlett <gabe@rundlett.com>
2022-04-04 09:45:18 -0600

    add keybind menu

--- DaxaMinecraft/src/game/graphics.hpp
+++ DaxaMinecraft/src/game/graphics.hpp
@@ -244,7 +244,7 @@ struct RenderableChunk {
 };
 
 struct World {
-    static constexpr glm::ivec3 DIM = glm::ivec3{2048, 256, 2048} / Chunk::DIM;
+    static constexpr glm::ivec3 DIM = glm::ivec3{512, 256, 512} / Chunk::DIM;
 
     template <typename T>
     using ChunkArray = std::array<std::array<std::array<T, DIM.x>, DIM.y>, DIM.z>;
