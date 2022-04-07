bc077043f5c477f7489a526efd9b5ed2c5d39cce
Ipotrick <patrick.ahrens.32369@web.de>
2022-04-07 04:17:43 +0200

    started work on taa, implemented filter and subscaling shader for fft bloom

--- DaxaMeshview/renderer/RenderContext.hpp
+++ DaxaMeshview/renderer/RenderContext.hpp
@@ -41,7 +41,6 @@ public:
     }
 
 	void recreateFramebuffer(daxa::CommandListHandle& cmd, u32 width, u32 height) {
-		std::string depthMapName = "depth image";
 		this->depthImage = device->createImageView({
 			.image = device->createImage({
 				.format = VK_FORMAT_D32_SFLOAT,
@@ -57,7 +56,24 @@ public:
 				.layerCount = 1,
 			},
 			.defaultSampler = defaultSampler,
-			.debugName = depthMapName.c_str(),//"depth image",
+			.debugName = "depth image",
+		});
+		this->priorFrameDepthImage = device->createImageView({
+			.image = device->createImage({
+				.format = VK_FORMAT_D32_SFLOAT,
+				.extent = { width, height, 1},
+				.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			}),
+			.format = VK_FORMAT_D32_SFLOAT,
+			.subresourceRange = {
+				.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1,
+			},
+			.defaultSampler = defaultSampler,
+			.debugName = "prior frame depth image"
 		});
 
 		this->normalsImage = device->createImageView({
@@ -81,23 +97,56 @@ public:
 			.defaultSampler = defaultSampler,
 			.debugName = "main hdr color image",
 		});
+		this->priorFrameHdrImage = device->createImageView({
+			.image = device->createImage({
+				.format = VK_FORMAT_R16G16B16A16_SFLOAT,
+				.extent = { width, height, 1 },
+				.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			}),
+			.format = VK_FORMAT_R16G16B16A16_SFLOAT,
+			.defaultSampler = defaultSampler,
+			.debugName = "prior frame hdr color image",
+		});
+		this->motionImage = device->createImageView({
+			.image = device->createImage({
+				.format = VK_FORMAT_R16G16_SFLOAT,
+				.extent = { width, height, 1 },
+				.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			}),
+			.format = VK_FORMAT_R16G16B16A16_SFLOAT,
+			.defaultSampler = defaultSampler,
+			.debugName = "prior frame hdr color image",
+		});
 
-		cmd->insertImageBarriers(std::array{
-			daxa::ImageBarrier{
-				.barrier = daxa::FULL_MEMORY_BARRIER,
-				.image = depthImage,
-				.layoutAfter = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL,
-			},
-			daxa::ImageBarrier{
-				.barrier = daxa::FULL_MEMORY_BARRIER,
-				.image = normalsImage,
-				.layoutAfter = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-			},
-			daxa::ImageBarrier{
-				.barrier = daxa::FULL_MEMORY_BARRIER,
-				.image = hdrImage,
-				.layoutAfter = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
-			},
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = depthImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL,
+		});
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = priorFrameDepthImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL,
+		});
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = normalsImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		});
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = hdrImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		});
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = priorFrameHdrImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		});
+		cmd->queueImageBarrier({
+			.barrier = daxa::FULL_MEMORY_BARRIER,
+			.image = motionImage,
+			.layoutAfter = VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL,
 		});
 	}
 
@@ -124,6 +173,11 @@ public:
 	daxa::ImageViewHandle normalsImage = {};
 	daxa::ImageViewHandle hdrImage = {};
 	daxa::SamplerHandle defaultSampler = {};
+
+	// used by TAA:
+	daxa::ImageViewHandle motionImage = {};
+	daxa::ImageViewHandle priorFrameHdrImage = {};
+	daxa::ImageViewHandle priorFrameDepthImage = {};
 };
 
 struct Primitive {
