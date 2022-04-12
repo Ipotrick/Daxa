42b2e1c3176425c2d393dfcfbdc1f5d06bbfe01c
GabeRundlett <gabe@rundlett.com>
2022-04-12 14:35:16 -0600

    visualize "subchunks"

--- Samples/Shadertoy/shaders/dda.hlsl
+++ Samples/Shadertoy/shaders/dda.hlsl
@@ -1,6 +1,9 @@
+#include "utils/noise.hlsl"
+
+#define CHUNK_SIZE 128
 
 int2 get_tile_i(float2 p) {
-    int2 tile_i = int2(p / 1) * 1;
+    int2 tile_i = int2(p);
     if (p.x < 0)
         tile_i.x -= 1;
     if (p.y < 0)
@@ -11,7 +14,7 @@ int2 get_tile_i(float2 p) {
 float2 pixelspace_to_worldspace(StructuredBuffer<Globals> globals, float2 p) {
     float2 uv = p / float2(globals[0].frame_dim) * 2 - 1;
     uv.x *= float(globals[0].frame_dim.x) / globals[0].frame_dim.y;
-    return uv * 10;
+    return uv * CHUNK_SIZE / 2;
 }
 
 float grid_dist(float2 p, float s, float t) {
@@ -74,12 +77,51 @@ void dda_setup_axis(inout float initial_dist, inout int tile_step, in int tile_i
     }
 }
 
-void dda(StructuredBuffer<Globals> globals, float2 pixel_pos, inout float3 color, Ray ray, int max_steps) {
-    float2 delta_dist = float2(                   //
-        ray.nrm.x == 0 ? 1 : abs(ray.inv_nrm.x),  //
-        ray.nrm.y == 0 ? 1 : abs(ray.inv_nrm.y));
+bool is_tile_occluding(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = tile_i + CHUNK_SIZE / 2;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE)
+        return false;
+    return buf0[0].data[tile_i.x + tile_i.y * CHUNK_SIZE];
+}
+bool is_tile_x2(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = (tile_i + CHUNK_SIZE / 2) / 2;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE / 2 || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE / 2)
+        return false;
+    return buf0[0].data_1[(tile_i.x + tile_i.y * CHUNK_SIZE / 2)];
+}
+bool is_tile_x4(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = (tile_i + CHUNK_SIZE / 2) / 4;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE / 4 || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE / 4)
+        return false;
+    return buf0[0].data_2[(tile_i.x + tile_i.y * CHUNK_SIZE / 4)];
+}
+bool is_tile_x8(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = (tile_i + CHUNK_SIZE / 2) / 8;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE / 8 || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE / 8)
+        return false;
+    return buf0[0].data_3[(tile_i.x + tile_i.y * CHUNK_SIZE / 8)];
+}
+bool is_tile_x16(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = (tile_i + CHUNK_SIZE / 2) / 16;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE / 16 || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE / 16)
+        return false;
+    return buf0[0].data_4[(tile_i.x + tile_i.y * CHUNK_SIZE / 16)];
+}
+bool is_tile_x32(in StructuredBuffer<Buf0> buf0, in int2 tile_i) {
+    tile_i = (tile_i + CHUNK_SIZE / 2) / 32;
+    if (tile_i.x < 0 || tile_i.x >= CHUNK_SIZE / 32 || tile_i.y < 0 || tile_i.y >= CHUNK_SIZE / 32)
+        return false;
+    return buf0[0].data_5[(tile_i.x + tile_i.y * CHUNK_SIZE / 32)];
+}
+
+void dda(StructuredBuffer<Globals> globals, float2 pixel_pos, inout float3 color, in StructuredBuffer<Buf0> buf0,
+         Ray ray, int max_steps) {
+    float2 delta_dist = float2(                           //
+        ray.nrm.x == 0 ? max_steps : abs(ray.inv_nrm.x),  //
+        ray.nrm.y == 0 ? max_steps : abs(ray.inv_nrm.y));
 
     int2 tile_i = get_tile_i(ray.o);
+    int2 pixel_tile_i = get_tile_i(pixel_pos);
 
     float2 initial_dist;
     int2 tile_step;
@@ -87,14 +129,21 @@ void dda(StructuredBuffer<Globals> globals, float2 pixel_pos, inout float3 color
     dda_setup_axis(initial_dist.x, tile_step.x, tile_i.x, ray.o.x, ray.nrm.x, delta_dist.x);
     dda_setup_axis(initial_dist.y, tile_step.y, tile_i.y, ray.o.y, ray.nrm.y, delta_dist.y);
 
-    int2 total_steps = int2(0, 0);
+    int2 total_axis_steps = int2(0, 0);
     int axis = 0;
 
-    color_add(color, float3(0.1, 0.1, 0.1), square_dist(pixel_pos, tile_i, 0.875) < 0);
-    color_add(color, float3(0.1, 0.1, 0.1), manhattan_dist(tile_i, get_tile_i(pixel_pos)) < max_steps);
+    color_set(color, float3(0.1, 0.2, 0.5), is_tile_occluding(buf0, pixel_tile_i));
+    // color_add(color, float3(0.1, 0.2, 0.5), is_tile_x8(buf0, pixel_tile_i));
 
     for (int i = 0; i < max_steps; ++i) {
-        float2 total_dist = initial_dist + delta_dist * total_steps;
+        int2 current_tile_i = tile_i + tile_step * total_axis_steps;
+        if (is_tile_occluding(buf0, current_tile_i)) {
+            color_set(color, float3(0.6, 0.1, 0.1), square_dist(pixel_pos, current_tile_i, 0.875) < 0);
+            break;
+        }
+        color_add(color, float3(0.1, 0.1, 0.1), square_dist(pixel_pos, current_tile_i, 0.875) < 0);
+
+        float2 total_dist = initial_dist + delta_dist * total_axis_steps;
 
         if (total_dist.x < total_dist.y) {
             axis = 0;
@@ -102,14 +151,16 @@ void dda(StructuredBuffer<Globals> globals, float2 pixel_pos, inout float3 color
             axis = 1;
         }
 
-        color_set(color, float3(0.9, 0.9, 0.9), circle_dist(pixel_pos, ray.o + total_dist[axis] * ray.nrm, 0.1) < 0);
+        float2 hit_p = ray.o + total_dist[axis] * ray.nrm;
+        color_set(color, float3(0.9, 0.9, 0.9), circle_dist(pixel_pos, hit_p, 0.1) < 0);
 
-        total_steps[axis] += 1;
+        total_axis_steps[axis] += 1;
     }
 }
 
-SHADERTOY_NUMTHREADS void main(uint3 pixel_i : SV_DispatchThreadID) {
+SHADERTOY_NUMTHREADS_MAIN void main(uint3 pixel_i : SV_DispatchThreadID) {
     StructuredBuffer<Globals> globals = getBuffer<Globals>(p.globals_id);
+    StructuredBuffer<Buf0> buf0 = getBuffer<Buf0>(p.buf0_id);
     RWTexture2D<float4> output_image = getRWTexture2D<float4>(p.output_image_id);
 
     float2 mouse_pos = pixelspace_to_worldspace(globals, globals[0].mouse_pos);
@@ -126,11 +177,90 @@ SHADERTOY_NUMTHREADS void main(uint3 pixel_i : SV_DispatchThreadID) {
     ray.nrm = normalize(rmb_pos - ray.o);
     ray.inv_nrm = 1 / ray.nrm;
 
-    color_set(color, float3(0.15, 0.15, 0.15), line_segment_dist(pixel_pos, ray.o, rmb_pos, 0.1) < 0);
-    color_set(color, float3(0.50, 0.20, 0.50), circle_dist(pixel_pos, ray.o, 0.1) < 0);
-    color_set(color, float3(0.25, 0.25, 0.25), circle_dist(pixel_pos, rmb_pos, 0.1) < 0);
+    color_set(color, float3(0.15, 0.15, 0.15), line_segment_dist(pixel_pos, ray.o, rmb_pos, 0.5) < 0);
+    color_set(color, float3(0.50, 0.20, 0.50), circle_dist(pixel_pos, ray.o, 0.5) < 0);
+    color_set(color, float3(0.25, 0.25, 0.25), circle_dist(pixel_pos, rmb_pos, 0.5) < 0);
 
-    dda(globals, pixel_pos, color, ray, 6);
+    dda(globals, pixel_pos, color, buf0, ray, CHUNK_SIZE * 2);
 
     output_image[pixel_i.xy] = float4(color, 1);
+}
+
+SHADERTOY_NUMTHREADS_BUF0 void buf0_main(uint3 tile_i : SV_DispatchThreadID) {
+    StructuredBuffer<Globals> globals = getBuffer<Globals>(p.globals_id);
+    StructuredBuffer<Buf0> buf0 = getBuffer<Buf0>(p.buf0_id);
+
+    int i = tile_i.x + tile_i.y * CHUNK_SIZE;
+
+    switch (p.output_image_id) {
+    case 0: {
+        FractalNoiseConfig noise_conf = {
+            /* .amplitude   = */ 1.0f,
+            /* .persistance = */ 0.4f,
+            /* .scale       = */ 0.1f,
+            /* .lacunarity  = */ 2,
+            /* .octaves     = */ 6,
+        };
+        buf0[0].data[i] = fractal_noise(float3(tile_i.xy, 0), noise_conf) - 0.5 < 0;
+    } break;
+    case 1: {
+        if (tile_i.x % 2 == 0 && tile_i.y % 2 == 0) {
+            i = tile_i.x / 2 + tile_i.y / 2 * CHUNK_SIZE / 2;
+            if (i < CHUNK_SIZE * CHUNK_SIZE / 2)
+                buf0[0].data_1[i] =  //
+                    buf0[0].data[tile_i.x + 0 + (tile_i.y + 0) * CHUNK_SIZE] ||
+                    buf0[0].data[tile_i.x + 1 + (tile_i.y + 0) * CHUNK_SIZE] ||
+                    buf0[0].data[tile_i.x + 0 + (tile_i.y + 1) * CHUNK_SIZE] ||
+                    buf0[0].data[tile_i.x + 1 + (tile_i.y + 1) * CHUNK_SIZE];
+        }
+    } break;
+    case 2: {
+        if (tile_i.x % 4 == 0 && tile_i.y % 4 == 0) {
+            i = tile_i.x / 4 + tile_i.y / 4 * CHUNK_SIZE / 4;
+            tile_i = tile_i / 2;
+            if (i < CHUNK_SIZE * CHUNK_SIZE / 4)
+                buf0[0].data_2[i] =  //
+                    buf0[0].data_1[tile_i.x + 0 + (tile_i.y + 0) * CHUNK_SIZE / 2] ||
+                    buf0[0].data_1[tile_i.x + 1 + (tile_i.y + 0) * CHUNK_SIZE / 2] ||
+                    buf0[0].data_1[tile_i.x + 0 + (tile_i.y + 1) * CHUNK_SIZE / 2] ||
+                    buf0[0].data_1[tile_i.x + 1 + (tile_i.y + 1) * CHUNK_SIZE / 2];
+        }
+    } break;
+    case 3: {
+        if (tile_i.x % 8 == 0 && tile_i.y % 8 == 0) {
+            i = tile_i.x / 8 + tile_i.y / 8 * CHUNK_SIZE / 8;
+            tile_i = tile_i / 4;
+            if (i < CHUNK_SIZE * CHUNK_SIZE / 8)
+                buf0[0].data_3[i] =  //
+                    buf0[0].data_2[tile_i.x + 0 + (tile_i.y + 0) * CHUNK_SIZE / 4] ||
+                    buf0[0].data_2[tile_i.x + 1 + (tile_i.y + 0) * CHUNK_SIZE / 4] ||
+                    buf0[0].data_2[tile_i.x + 0 + (tile_i.y + 1) * CHUNK_SIZE / 4] ||
+                    buf0[0].data_2[tile_i.x + 1 + (tile_i.y + 1) * CHUNK_SIZE / 4];
+        }
+    } break;
+    case 4: {
+        if (tile_i.x % 16 == 0 && tile_i.y % 16 == 0) {
+            i = tile_i.x / 16 + tile_i.y / 16 * CHUNK_SIZE / 16;
+            tile_i = tile_i / 8;
+            if (i < CHUNK_SIZE * CHUNK_SIZE / 16)
+                buf0[0].data_4[i] =  //
+                    buf0[0].data_3[tile_i.x + 0 + (tile_i.y + 0) * CHUNK_SIZE / 8] ||
+                    buf0[0].data_3[tile_i.x + 1 + (tile_i.y + 0) * CHUNK_SIZE / 8] ||
+                    buf0[0].data_3[tile_i.x + 0 + (tile_i.y + 1) * CHUNK_SIZE / 8] ||
+                    buf0[0].data_3[tile_i.x + 1 + (tile_i.y + 1) * CHUNK_SIZE / 8];
+        }
+    } break;
+    case 5: {
+        if (tile_i.x % 32 == 0 && tile_i.y % 32 == 0) {
+            i = tile_i.x / 32 + tile_i.y / 32 * CHUNK_SIZE / 32;
+            tile_i = tile_i / 16;
+            if (i < CHUNK_SIZE * CHUNK_SIZE / 32)
+                buf0[0].data_5[i] =  //
+                    buf0[0].data_4[tile_i.x + 0 + (tile_i.y + 0) * CHUNK_SIZE / 16] ||
+                    buf0[0].data_4[tile_i.x + 1 + (tile_i.y + 0) * CHUNK_SIZE / 16] ||
+                    buf0[0].data_4[tile_i.x + 0 + (tile_i.y + 1) * CHUNK_SIZE / 16] ||
+                    buf0[0].data_4[tile_i.x + 1 + (tile_i.y + 1) * CHUNK_SIZE / 16];
+        }
+    } break;
+    }
 }
