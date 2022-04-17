605160f48cfe4bcc319aed1fe03b545bd63e607b
GabeRundlett <gabe@rundlett.com>
2022-04-17 09:48:04 -0600

    Fix some warnings

--- DaxaMeshview/AssetServer.hpp
+++ DaxaMeshview/AssetServer.hpp
@@ -108,7 +108,7 @@ public:
 
     daxa::BufferHandle loadBuffer(daxa::CommandListHandle& cmdList, cgltf_accessor& accessor, VkBufferUsageFlagBits usage) {
 
-        VkBufferUsageFlags usageFlags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | usage;
+        // VkBufferUsageFlags usageFlags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | usage;
 
         daxa::BufferHandle gpuBuffer;
 
