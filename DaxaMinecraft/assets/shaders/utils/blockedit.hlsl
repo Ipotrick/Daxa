ad50414bcc33b273e2e4b5ed6bc65e394ae183a6
GabeRundlett <gabe@rundlett.com>
2022-04-19 15:52:07 -0600

    Fixed array access out of bounds in blockedit

--- DaxaMinecraft/assets/shaders/utils/blockedit.hlsl
+++ DaxaMinecraft/assets/shaders/utils/blockedit.hlsl
@@ -17,13 +17,15 @@ struct Push {
     int xi = pick_chunk_i.x + p.pos.x;
     int yi = pick_chunk_i.y + p.pos.y;
     int zi = pick_chunk_i.z + p.pos.z;
-    uint chunk_id = globals[0].chunk_images[zi][yi][xi];
-    float3 p_pos = float3(1.0f * xi * CHUNK_SIZE, 1.0f * yi * CHUNK_SIZE, 1.0f * zi * CHUNK_SIZE);
-    float3 bp = float3(global_i) + p_pos;
-    uint chunk_texture_id = chunk_id;
-    RWTexture3D<uint> chunk = getRWTexture3D<uint>(chunk_texture_id);
+    if (xi >= 0 && xi < CHUNK_NX && yi >= 0 && yi < CHUNK_NY && zi >= 0 && zi < CHUNK_NZ) {
+        uint chunk_id = globals[0].chunk_images[zi][yi][xi];
+        float3 p_pos = float3(1.0f * xi * CHUNK_SIZE, 1.0f * yi * CHUNK_SIZE, 1.0f * zi * CHUNK_SIZE);
+        float3 bp = float3(global_i) + p_pos;
+        uint chunk_texture_id = chunk_id;
+        RWTexture3D<uint> chunk = getRWTexture3D<uint>(chunk_texture_id);
 
-    if (length(int3(bp) - int3(globals[0].pick_pos[0].xyz)) <= 0.5 + BLOCKEDIT_RADIUS) {
-        chunk[int3(global_i)] = p.set_id;
+        if (length(int3(bp) - int3(globals[0].pick_pos[0].xyz)) <= 0.5 + BLOCKEDIT_RADIUS) {
+            chunk[int3(global_i)] = p.set_id;
+        }
     }
 }
