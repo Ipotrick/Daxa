bdf8aa4957a281b30c722151230a0606ef736c1b
GabeRundlett <gabe@rundlett.com>
2022-04-30 11:03:59 -0600

    Only warn on build, don't propogate when installing

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -70,6 +70,6 @@ function(set_project_warnings project_name)
     message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
   endif()
 
-  target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
+  target_compile_options(${project_name} INTERFACE $<BUILD_INTERFACE:${PROJECT_WARNINGS}>)
 
 endfunction()
