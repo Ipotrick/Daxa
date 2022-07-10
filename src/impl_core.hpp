2a570b3122a51da6875cbc64dcec0584165fccf5
Ipotrick <patrick.ahrens.32369@web.de>
2022-07-10 19:33:18 +0200

    worked on device creation

--- src/impl_core.hpp
+++ src/impl_core.hpp
@@ -1,4 +1,9 @@
 #pragma once
 
+#include <unordered_map>
+
+#include <volk.h>
+#define VK_NO_PROTOTYPES
 #include <vulkan/vulkan.h>
 
+#include <daxa/core.hpp>
--- src/impl_device.hpp
+++ src/impl_device.hpp
@@ -10,10 +10,12 @@ namespace daxa
 {
     struct ImplDevice
     {
-        VkDevice vk_device_handle;
-        std::shared_ptr<ImplContext> ctx;
-        
-        ImplDevice();
+        VkDevice vk_device_handle = {};
+        VkQueue vk_main_queue_handle = {};
+        std::shared_ptr<ImplContext> ctx = {};
+        DeviceInfo info = {};
+
+        ImplDevice(std::shared_ptr<ImplContext> impl_ctx, VkPhysicalDevice physical_device);
         ~ImplDevice();
     };
-}
+} // namespace daxa
