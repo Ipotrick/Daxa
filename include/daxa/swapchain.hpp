46b99cf98ff4b2ec0d4b1348c12f9e46c3d30999
GabeRundlett <gabe@rundlett.com>
2022-07-13 09:07:41 -0600

    Fix warnings and add PipelineCompiler skeleton

--- include/daxa/swapchain.hpp
+++ include/daxa/swapchain.hpp
@@ -7,8 +7,8 @@ namespace daxa
     struct SwapchainInfo
     {
         NativeWindowHandle native_window_handle;
-        u32 width = 64;
-        u32 height = 64;
+        u32 width = 0;
+        u32 height = 0;
         PresentMode present_mode = PresentMode::DOUBLE_BUFFER_WAIT_FOR_VBLANK;
         PresentOp present_operation = PresentOp::IDENTITY;
         ImageUsageFlags image_usage = {};
@@ -17,12 +17,13 @@ namespace daxa
 
     struct Swapchain : Handle
     {
-        auto info() const -> SwapchainInfo const&;
+        auto info() const -> SwapchainInfo const &;
 
         void resize(u32 width, u32 height);
         void change_present_mode(PresentMode new_present_mode);
-    private:
+
+      private:
         friend struct Device;
         Swapchain(std::shared_ptr<void> impl);
     };
-}
+} // namespace daxa
