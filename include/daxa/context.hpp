f09ee91cc244fa160a796c3752765823fb01b224
GabeRundlett <gabe@rundlett.com>
2022-07-17 11:44:50 -0600

    Add tests!

--- include/daxa/context.hpp
+++ include/daxa/context.hpp
@@ -5,10 +5,24 @@
 
 namespace daxa
 {
+    static inline void default_validation_callback(daxa::MsgSeverity severity, daxa::MsgType, std::string_view msg)
+    {
+        switch (severity)
+        {
+        // clang-format off
+        case daxa::MsgSeverity::VERBOSE: break;
+        case daxa::MsgSeverity::INFO:    break;
+        case daxa::MsgSeverity::WARNING: std::cout << '\n' << "[WARNING]: " << msg << '\n' <<std::endl; DAXA_DBG_ASSERT_TRUE_M(false, "validation warning"); break;
+        case daxa::MsgSeverity::FAILURE: std::cout << '\n' << "[FAILURE]: " << msg << '\n' <<std::endl; DAXA_DBG_ASSERT_TRUE_M(false, "validation error"); break;
+        // clang-format on
+        default: std::cout << "[UNKNOWN]: " << msg << std::endl; break;
+        }
+    }
+
     struct ContextInfo
     {
         bool enable_validation = false;
-        std::function<void(MsgSeverity, MsgType, std::string_view)> validation_callback;
+        std::function<void(MsgSeverity, MsgType, std::string_view)> validation_callback = default_validation_callback;
     };
 
     struct Context : Handle
