eae338bf067512ab93aa03f69e6b91d3be3d5e92
GabeRundlett <gabe@rundlett.com>
2022-07-17 11:44:50 -0600

    Add tests!

---
+++ tests/1_setup/1_window/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <thread>
+
+#include <GLFW/glfw3.h>
+#if defined(_WIN32)
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif defined(__linux__)
+#define GLFW_EXPOSE_NATIVE_X11
+#endif
+#include <GLFW/glfw3native.h>
+
+#include <daxa/daxa.hpp>
+using namespace daxa::types;
+
+template <typename App>
+struct AppWindow
+{
+    GLFWwindow * glfw_window_ptr;
+    u32 size_x = 800, size_y = 600;
+    bool minimized = false;
+
+    AppWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfw_window_ptr = glfwCreateWindow(static_cast<i32>(size_x), static_cast<i32>(size_y), "test1", nullptr, nullptr);
+        glfwSetWindowUserPointer(glfw_window_ptr, this);
+        glfwSetWindowSizeCallback(
+            glfw_window_ptr,
+            [](GLFWwindow * window_ptr, i32 sx, i32 sy)
+            {
+                auto & app = *reinterpret_cast<App *>(glfwGetWindowUserPointer(window_ptr));
+                app.on_resize(static_cast<u32>(sx), static_cast<u32>(sy));
+            });
+    }
+};
+
+struct App : AppWindow<App>
+{
+    App() {}
+    ~App() {}
+
+    bool update()
+    {
+        glfwPollEvents();
+        if (glfwWindowShouldClose(glfw_window_ptr))
+        {
+            return true;
+        }
+        if (!minimized)
+        {
+            draw();
+        }
+        else
+        {
+            using namespace std::literals;
+            std::this_thread::sleep_for(1ms);
+        }
+        return false;
+    }
+
+    void draw()
+    {
+    }
+
+    void on_resize(u32 sx, u32 sy)
+    {
+        size_x = sx;
+        size_y = sy;
+        minimized = (sx == 0 || sy == 0);
+        if (!minimized)
+        {
+            draw();
+        }
+    }
+};
+
+int main()
+{
+    App app = {};
+    while (true)
+    {
+        if (app.update())
+            break;
+    }
+}
