6a9034fe1498e917b7c4a05bed5a567d39fd7bdf
Ipotrick <patrick.ahrens.32369@web.de>
2022-07-19 00:05:57 +0200

    added automatic batching of pipeline barrier calls

--- src/impl_command_list.hpp
+++ src/impl_command_list.hpp
@@ -16,6 +16,8 @@ namespace daxa
     static inline constexpr usize DEFERRED_DESTRUCTION_SAMPLER_INDEX = 3;
     static inline constexpr usize DEFERRED_DESTRUCTION_COUNT_MAX = 32;
 
+    static inline constexpr usize COMMAND_LIST_BARRIER_MAX_BATCH_SIZE = 16;
+
     struct ImplCommandList
     {
         using InfoT = CommandListInfo;
@@ -25,9 +27,15 @@ namespace daxa
         VkCommandBuffer vk_cmd_buffer = {};
         VkCommandPool vk_cmd_pool = {};
         bool recording_complete = true;
+        std::array<VkMemoryBarrier2, COMMAND_LIST_BARRIER_MAX_BATCH_SIZE> memory_barrier_batch = {};
+        std::array<VkImageMemoryBarrier2, COMMAND_LIST_BARRIER_MAX_BATCH_SIZE> image_barrier_batch = {};
+        usize image_barrier_batch_count = 0;
+        usize memory_barrier_batch_count = 0;
         std::array<VkPipelineLayout, PIPELINE_LAYOUT_COUNT> pipeline_layouts = {};
         std::array<std::pair<GPUResourceId, u8>, DEFERRED_DESTRUCTION_COUNT_MAX> deferred_destructions = {};
         usize deferred_destruction_count = {};
+
+        void flush_barriers();
 
         ImplCommandList(std::weak_ptr<ImplDevice> device_impl);
         ~ImplCommandList();
