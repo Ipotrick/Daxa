f8a523eb4550994f867854f252693576a3d821cf
Ipotrick <patrick.ahrens.32369@web.de>
2022-07-18 23:50:50 +0200

    fixed bug

--- src/impl_command_list.hpp
+++ src/impl_command_list.hpp
@@ -10,6 +10,12 @@ namespace daxa
 {
     struct ImplDevice;
 
+    static inline constexpr usize DEFERRED_DESTRUCTION_BUFFER_INDEX = 0;
+    static inline constexpr usize DEFERRED_DESTRUCTION_IMAGE_INDEX = 1;
+    static inline constexpr usize DEFERRED_DESTRUCTION_IMAGE_VIEW_INDEX = 2;
+    static inline constexpr usize DEFERRED_DESTRUCTION_SAMPLER_INDEX = 3;
+    static inline constexpr usize DEFERRED_DESTRUCTION_COUNT_MAX = 32;
+
     struct ImplCommandList
     {
         using InfoT = CommandListInfo;
@@ -20,6 +26,8 @@ namespace daxa
         VkCommandPool vk_cmd_pool = {};
         bool recording_complete = true;
         std::array<VkPipelineLayout, PIPELINE_LAYOUT_COUNT> pipeline_layouts = {};
+        std::array<std::pair<GPUResourceId, u8>, DEFERRED_DESTRUCTION_COUNT_MAX> deferred_destructions = {};
+        usize deferred_destruction_count = {};
 
         ImplCommandList(std::weak_ptr<ImplDevice> device_impl);
         ~ImplCommandList();
