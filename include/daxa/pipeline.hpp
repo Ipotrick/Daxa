8271f683c62b240d0e469e864069776b978fe335
Ipotrick <patrick.ahrens.32369@web.de>
2022-07-20 22:47:49 +0200

    started raster pipeline

--- include/daxa/pipeline.hpp
+++ include/daxa/pipeline.hpp
@@ -8,10 +8,13 @@ namespace daxa
     {
         std::filesystem::path path;
     };
+
+    // This string will only work if it is valid HLSL
     struct ShaderCode
     {
         std::string string;
     };
+
     struct ShaderSPIRV
     {
         u32 * data;
@@ -23,8 +26,15 @@ namespace daxa
     struct ShaderInfo
     {
         ShaderSource source;
-        std::string entry_point = { "main" };
+        std::string entry_point = {"main"};
         std::vector<std::string> defines = {};
+        std::string debug_name = {};
+    };
+
+    struct GraphicsPipelineInfo
+    {
+        ShaderInfo shader_info = {};
+        u32 push_constant_size = {};
         std::string debug_name = {};
     };
 
@@ -47,6 +57,73 @@ namespace daxa
         ComputePipeline(std::shared_ptr<void> impl);
     };
 
+    struct DepthTestInfo
+    {
+		Format 	    depthAttachmentFormat 	= {};
+		bool 		enableDepthTest 		= false;
+		bool 		enableDepthWrite 		= false;
+		CompareOp   depthTestCompareOp 		= {};
+		f32 		minDepthBounds 			= 0.0f;
+		f32 		maxDepthBounds 			= 1.0f;
+    };
+
+    enum class PolygonMode
+    {
+
+    };
+
+    using FaceCullFlags = u32;
+    struct FaceCullFlagBits
+    {
+
+    };
+
+    struct RasterizerInfo
+    {
+        PolygonMode polygon_mode = {};
+        FaceCullFlags face_culling = {};
+        bool depthClampEnable = false;
+        bool rasterizerDiscardEnable = false;
+        bool depthBiasEnable = false;
+        f32 depthBiasConstantFactor = 0.0f;
+        f32 depthBiasClamp = 0.0f;
+        f32 depthBiasSlopeFactor = 0.0f;
+        f32 lineWidth = 1.0f;
+    };
+
+    struct BlendInfo
+    {
+    };
+
+    struct RenderAttachment
+    {
+        Format format = {};
+        BlendInfo blend = {};
+    };
+
+    struct RasterPipelineInfo
+    {
+        ShaderInfo vertex_shader_info = {};
+        ShaderInfo fragment_shader_info = {};
+        std::vector<RenderAttachment> render_attachment = {};
+        DepthTestInfo depth_test = {};
+        RasterizerInfo raster = {};
+        u32 push_constant_size = {};
+        std::string debug_name = {};
+    };
+
+    struct RasterPipeline : Handle
+    {
+        ~RasterPipeline();
+
+        auto info() const -> RasterPipelineInfo const &;
+
+      private:
+        friend struct PipelineCompiler;
+        friend struct CommandList;
+        RasterPipeline(std::shared_ptr<void> impl);
+    };
+
     struct PipelineCompilerInfo
     {
         std::vector<std::filesystem::path> root_paths = {};
@@ -55,9 +132,12 @@ namespace daxa
 
     struct PipelineCompiler : Handle
     {
+        auto create_graphics_pipeline(GraphicsPipelineInfo const & info) -> Result<GraphicsPipeline>;
+        auto recreate_graphics_pipeline(GraphicsPipeline const & pipeline) -> Result<GraphicsPipeline>;
         auto create_compute_pipeline(ComputePipelineInfo const & info) -> Result<ComputePipeline>;
-        auto recreate_compute_pipeline(ComputePipeline const &pipeline) -> Result<ComputePipeline>;
-        auto check_if_sources_changed(ComputePipeline const &pipeline) -> bool;
+        auto recreate_compute_pipeline(ComputePipeline const & pipeline) -> Result<ComputePipeline>;
+        auto check_if_sources_changed(GraphicsPipeline const & pipeline) -> bool;
+        auto check_if_sources_changed(ComputePipeline const & pipeline) -> bool;
 
       private:
         friend struct Device;
