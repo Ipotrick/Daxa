9a1fd1504f66b33e42dfc74414595c163a2a70e5
Ipotrick <patrick.ahrens.32369@web.de>
2022-07-23 01:50:28 +0200

    big progress on task list

--- src/impl_core.cpp
+++ src/impl_core.cpp
@@ -3,4 +3,186 @@
 namespace daxa
 {
     Handle::Handle(std::shared_ptr<void> a_impl) : impl{std::move(a_impl)} {}
+    
+    auto Access::operator | (Access const & other) -> Access
+    {
+        Access{ .stage = this->stage | other.stage, .type = this->type | other.type };
+    }
+
+    auto Access::operator & (Access const & other) -> Access
+    {
+        Access{ .stage = this->stage & other.stage, .type = this->type & other.type };
+    }
+
+    auto ImageMipArraySlice::whole(ImageAspectFlags aspect) -> ImageMipArraySlice
+    {
+        return ImageMipArraySlice{
+            .image_aspect = aspect,
+            .base_mip_level = std::numeric_limits<u32>::max(),
+            .base_array_layer = std::numeric_limits<u32>::max(),
+        };
+    }
+
+    auto to_string(Access const & access) -> std::string_view
+    {
+        switch(access.stage) {
+        case AccessConsts::NONE.stage: return std::string_view{"NONE"};
+
+        case AccessConsts::TOP_OF_PIPE_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"TOP_OF_PIPE_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"TOP_OF_PIPE_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"TOP_OF_PIPE_READ_WRITE"};
+            }
+        case AccessConsts::DRAW_INDIRECT_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"DRAW_INDIRECT_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"DRAW_INDIRECT_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"DRAW_INDIRECT_READ_WRITE"};
+            }
+        case AccessConsts::VERTEX_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"VERTEX_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"VERTEX_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"VERTEX_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::TESSELLATION_CONTROL_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"TESSELLATION_CONTROL_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"TESSELLATION_CONTROL_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"TESSELLATION_CONTROL_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::TESSELLATION_EVALUATION_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"TESSELLATION_EVALUATION_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"TESSELLATION_EVALUATION_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"TESSELLATION_EVALUATION_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::GEOMETRY_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"GEOMETRY_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"GEOMETRY_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"GEOMETRY_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::FRAGMENT_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"FRAGMENT_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"FRAGMENT_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"FRAGMENT_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::EARLY_FRAGMENT_TESTS_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"EARLY_FRAGMENT_TESTS_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"EARLY_FRAGMENT_TESTS_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"EARLY_FRAGMENT_TESTS_READ_WRITE"};
+            }
+        case AccessConsts::LATE_FRAGMENT_TESTS_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"LATE_FRAGMENT_TESTS_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"LATE_FRAGMENT_TESTS_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"LATE_FRAGMENT_TESTS_READ_WRITE"};
+            }
+        case AccessConsts::COLOR_ATTACHMENT_OUTPUT_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"COLOR_ATTACHMENT_OUTPUT_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"COLOR_ATTACHMENT_OUTPUT_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"COLOR_ATTACHMENT_OUTPUT_READ_WRITE"};
+            }
+        case AccessConsts::COMPUTE_SHADER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"COMPUTE_SHADER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"COMPUTE_SHADER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"COMPUTE_SHADER_READ_WRITE"};
+            }
+        case AccessConsts::TRANSFER_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"TRANSFER_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"TRANSFER_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"TRANSFER_READ_WRITE"};
+            }
+        case AccessConsts::BOTTOM_OF_PIPE_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"BOTTOM_OF_PIPE_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"BOTTOM_OF_PIPE_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"BOTTOM_OF_PIPE_READ_WRITE"};
+            }
+        case AccessConsts::HOST_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"HOST_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"HOST_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"HOST_READ_WRITE"};
+            }
+        case AccessConsts::ALL_GRAPHICS_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"ALL_GRAPHICS_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"ALL_GRAPHICS_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"ALL_GRAPHICS_READ_WRITE"};
+            }
+        case AccessConsts::READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"READ_WRITE"};
+            }
+        case AccessConsts::COPY_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"COPY_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"COPY_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"COPY_READ_WRITE"};
+            }
+        case AccessConsts::RESOLVE_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"RESOLVE_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"RESOLVE_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"RESOLVE_READ_WRITE"};
+            }
+        case AccessConsts::BLIT_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"BLIT_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"BLIT_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"BLIT_READ_WRITE"};
+            }
+        case AccessConsts::CLEAR_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"CLEAR_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"CLEAR_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"CLEAR_READ_WRITE"};
+            }
+        case AccessConsts::INDEX_INPUT_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"INDEX_INPUT_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"INDEX_INPUT_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"INDEX_INPUT_READ_WRITE"};
+            }
+        case AccessConsts::PRE_RASTERIZATION_SHADERS_READ.stage:
+            switch(access.type)
+            {
+                case AccessTypeFlagBits::READ: return std::string_view{"PRE_RASTERIZATION_SHADERS_READ"};
+                case AccessTypeFlagBits::WRITE: return std::string_view{"PRE_RASTERIZATION_SHADERS_WRITE"};
+                case AccessTypeFlagBits::READ_WRITE: return std::string_view{"PRE_RASTERIZATION_SHADERS_READ_WRITE"};
+            }
+        default: return std::string_view{"unknown or composite access"}; break;
+        }
+    }
 } // namespace daxa
--- src/impl_task_list.hpp
+++ src/impl_task_list.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "impl_core.hpp"
 
 #include <daxa/task_list.hpp>
@@ -6,14 +8,60 @@ namespace daxa
 {
     struct ImplDevice;
 
+    struct ImplTaskBuffer
+    {
+        Access last_access = AccessConsts::NONE;
+        usize last_access_task_index = {};
+        BufferId runtime_id = {};
+    };
+
+    struct ImplTaskImage
+    {
+        Access last_access = AccessConsts::NONE;
+        usize last_access_task_index = {};
+        ImageId runtime_id = {};
+        ImageViewId runtime_view_id = {};
+    };
+
+    using TaskInfoVariant = std::variant<TaskInfo, TaskRenderInfo, std::monostate>;
+
+    struct TaskPipelineBarrier
+    {
+        bool with_image_transition = {};
+    };
+
+    struct ImplTask{
+        TaskInfoVariant info = {};
+        std::vector<TaskPipelineBarrier> barriers = {};
+    };
+
     struct ImplTaskList
     {
-        std::shared_ptr<ImplDevice> impl_device = {};
-        TaskListInfo info = {};
+        TaskListInfo info;
+        std::weak_ptr<ImplDevice> impl_device = {};
 
-        auto get_buffer(BufferId) -> BufferId;
+        bool compiled = false;
+        std::vector<ImplTask> tasks = {};
+        usize last_task_with_barrier = 0;
+
+        u32 next_task_buffer_id = 0;
+        u32 next_task_image_id = 0;
+        u32 next_task_image_view_id = 0;
+        std::vector<ImplTaskBuffer> impl_task_buffers = {};
+        std::vector<ImplTaskImage> impl_task_images = {};
+
+        auto access_to_image_layout(Access const & access) -> ImageLayout;
+        auto compute_needed_barrier(Access const & previous_access, Access const & new_access) -> std::optional<TaskPipelineBarrier>;
+
+        auto slot(TaskBufferId id) -> ImplTaskBuffer &;
+        auto slot(TaskImageId id) -> ImplTaskImage &;
+
+        auto get_buffer(TaskBufferId) -> BufferId;
         auto get_image(TaskImageId) -> ImageId;
-        auto get_image_view(TaskImageViewId) -> ImageViewId;
+        auto get_image_view(TaskImageId) -> ImageViewId;
+
+        void analyze_dependencies();
+        auto insert_synchonization();
 
         ImplTaskList(std::shared_ptr<ImplDevice> a_impl_device, TaskListInfo const & info);
         ~ImplTaskList();
