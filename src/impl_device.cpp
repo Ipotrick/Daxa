19637f42316be83f46e733b7ffe49fc09ea3b7e5
GabeRundlett <gabe@rundlett.com>
2022-08-01 10:55:35 -0600

    fix bad dynamic cast (all tests appear to run fine now!)

--- src/impl_device.cpp
+++ src/impl_device.cpp
@@ -71,7 +71,7 @@ namespace daxa
 
         for (auto & binary_semaphore : submit_info.wait_binary_semaphores)
         {
-            auto & impl_binary_semaphore = *binary_semaphore.as<ImplTimelineSemaphore>();
+            auto & impl_binary_semaphore = *binary_semaphore.as<ImplBinarySemaphore>();
             submit_semaphore_waits.push_back(impl_binary_semaphore.vk_semaphore);
             submit_semaphore_wait_stage_masks.push_back(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
             submit_semaphore_wait_values.push_back(0);
