6887789a3944e6276a3525c795be1cc7b8f1f250
Ipotrick <patrick.ahrens.32369@web.de>
2022-08-07 23:15:20 +0200

    added conditional task scopes synch backend

--- src/utils/impl_imgui.hpp
+++ src/utils/impl_imgui.hpp
@@ -11,13 +11,11 @@ namespace daxa
         RasterPipeline raster_pipeline;
         BufferId vbuffer, staging_vbuffer;
         BufferId ibuffer, staging_ibuffer;
-
-        std::unordered_map<u32, size_t> tex_handle_ptr_to_referenced_image_index;
-        std::vector<ImageId> referenced_images;
+        SamplerId sampler;
+        ImageId font_sheet;
 
         void recreate_vbuffer(usize vbuffer_new_size);
         void recreate_ibuffer(usize ibuffer_new_size);
-        auto get_imgui_texture_id(ImageId img) -> u64;
         void record_commands(ImDrawData * draw_data, CommandList & cmd_list, ImageId target_image, u32 size_x, u32 size_y);
         auto managed_cleanup() -> bool override;
 
