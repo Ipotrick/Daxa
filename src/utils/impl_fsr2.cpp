516a52b83ad3eecb80b49f6ffc1ef7d2775ee3ed
GabeRundlett <gabe@rundlett.com>
2022-08-14 13:36:28 -0600

    fix warnings

--- src/utils/impl_fsr2.cpp
+++ src/utils/impl_fsr2.cpp
@@ -98,21 +98,27 @@ namespace daxa
         auto & output_slot = impl_device.slot(info.output);
         auto & output_view_slot = impl_device.slot(info.output.default_view());
 
+        wchar_t fsr_inputcolor[] = L"FSR2_InputColor";
+        wchar_t fsr_inputdepth[] = L"FSR2_InputDepth";
+        wchar_t fsr_inputmotionvectors[] = L"FSR2_InputMotionVectors";
+        wchar_t fsr_inputexposure[] = L"FSR2_InputExposure";
+        wchar_t fsr_outputupscaledcolor[] = L"FSR2_OutputUpscaledColor";
+
         FfxFsr2DispatchDescription dispatch_description = {};
         dispatch_description.commandList = ffxGetCommandListVK(impl_command_list.vk_cmd_buffer);
         dispatch_description.color = ffxGetTextureResourceVK(
             &fsr2_context, color_slot.vk_image, color_view_slot.vk_image_view,
             this->info.size_info.render_size_x, this->info.size_info.render_size_y,
-            static_cast<VkFormat>(color_slot.info.format), L"FSR2_InputColor");
+            static_cast<VkFormat>(color_slot.info.format), fsr_inputcolor);
         dispatch_description.depth = ffxGetTextureResourceVK(
             &fsr2_context, depth_slot.vk_image, depth_view_slot.vk_image_view,
             this->info.size_info.render_size_x, this->info.size_info.render_size_y,
-            static_cast<VkFormat>(depth_slot.info.format), L"FSR2_InputDepth");
+            static_cast<VkFormat>(depth_slot.info.format), fsr_inputdepth);
         dispatch_description.motionVectors = ffxGetTextureResourceVK(
             &fsr2_context, motion_vectors_slot.vk_image, motion_vectors_view_slot.vk_image_view,
             this->info.size_info.render_size_x, this->info.size_info.render_size_y,
-            static_cast<VkFormat>(motion_vectors_slot.info.format), L"FSR2_InputMotionVectors");
-        dispatch_description.exposure = ffxGetTextureResourceVK(&fsr2_context, nullptr, nullptr, 1, 1, VK_FORMAT_UNDEFINED, L"FSR2_InputExposure");
+            static_cast<VkFormat>(motion_vectors_slot.info.format), fsr_inputmotionvectors);
+        dispatch_description.exposure = ffxGetTextureResourceVK(&fsr2_context, nullptr, nullptr, 1, 1, VK_FORMAT_UNDEFINED, fsr_inputexposure);
         // dispatch_description.reactive = ffxGetTextureResourceVK(&fsr2_context, nullptr, nullptr, 1, 1, VK_FORMAT_UNDEFINED, L"FSR2_InputReactiveMap");
         // dispatch_description.transparencyAndComposition = ffxGetTextureResourceVK(&fsr2_context, nullptr, nullptr, 1, 1, VK_FORMAT_UNDEFINED, L"FSR2_TransparencyAndCompositionMap");
         // dispatch_description.reactive = ffxGetTextureResourceVK(
@@ -126,7 +132,7 @@ namespace daxa
         dispatch_description.output = ffxGetTextureResourceVK(
             &fsr2_context, output_slot.vk_image, output_view_slot.vk_image_view,
             this->info.size_info.display_size_x, this->info.size_info.display_size_x,
-            static_cast<VkFormat>(output_slot.info.format), L"FSR2_OutputUpscaledColor",
+            static_cast<VkFormat>(output_slot.info.format), fsr_outputupscaledcolor,
             FFX_RESOURCE_STATE_UNORDERED_ACCESS);
         dispatch_description.jitterOffset.x = info.jitter.x;
         dispatch_description.jitterOffset.y = info.jitter.y;
