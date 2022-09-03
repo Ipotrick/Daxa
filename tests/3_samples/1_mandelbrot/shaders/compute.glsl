d2cb34caa85036050ef33443f721f63517641bbd
Ipotrick <patrick.ahrens.32369@web.de>
2022-09-04 00:33:28 +0200

    reformat

--- tests/3_samples/1_mandelbrot/shaders/compute.glsl
+++ tests/3_samples/1_mandelbrot/shaders/compute.glsl
@@ -10,5 +10,9 @@ void main()
     u32vec3 pixel_i = gl_GlobalInvocationID.xyz;
     if (pixel_i.x >= daxa_push.frame_dim.x || pixel_i.y >= daxa_push.frame_dim.y)
         return;
-    imageStore(daxa_GetRWImage(image2D, rgba32f, daxa_push.image_id), i32vec2(pixel_i.xy), f32vec4(1, 0, 1, 1));
+    imageStore(
+        daxa_GetRWImage(image2D, rgba32f, daxa_push.image_id), 
+        i32vec2(pixel_i.xy), 
+        f32vec4(1, 0, 1, 1)
+    );
 }
