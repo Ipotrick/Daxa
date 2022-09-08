7de5c85976b66d8f548fbfff7d867101cdba8766
GabeRundlett <gabe@rundlett.com>
2022-09-08 10:23:14 -0600

    Formatting, Build Fixes, some Runtime fixes (LINUX)

--- tests/3_samples/5_boids/shaders/vert.glsl
+++ tests/3_samples/5_boids/shaders/vert.glsl
@@ -17,9 +17,9 @@ void main()
     vec2 corner_position;
     switch (gl_VertexIndex % 3)
     {
-        case 0: corner_position = vec2(-1.0f, -1.0f); break;
-        case 1: corner_position = vec2( 1.0f, -1.0f); break;
-        case 2: corner_position = vec2( 0.0f,  1.0f); break;
+    case 0: corner_position = vec2(-1.0f, -1.0f); break;
+    case 1: corner_position = vec2(1.0f, -1.0f); break;
+    case 2: corner_position = vec2(0.0f, 1.0f); break;
     }
 
     vec2 direction = push_constant.boids_buffer.boids[boid_index].direction;
@@ -29,17 +29,12 @@ void main()
 
     corner_position = vec2(
         dot(corner_position, vec2(direction.x, -direction.y)),
-        dot(corner_position, vec2(direction.y, direction.x))
-    );
+        dot(corner_position, vec2(direction.y, direction.x)));
 
     corner_position *= BOID_SCALE;
-
     corner_position += push_constant.boids_buffer.boids[boid_index].position;
-
     corner_position *= 2.0f / (FIELD_SIZE);
-
     corner_position -= vec2(1.0f);
-
     corner_position *= push_constant.axis_scaling;
 
     gl_Position = vec4(corner_position, 0.0f, 1.0f);
