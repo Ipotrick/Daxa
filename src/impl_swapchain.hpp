145103e2a90256b42317f61013f2ade21040c879
Patrick Ahrens <patrick.ahrens.32369@web.de>
2022-10-04 01:38:07 +0200

    Added internal and updated external swapchain docs. Fixed bug in swapchain internals.

--- src/impl_swapchain.hpp
+++ src/impl_swapchain.hpp
@@ -8,6 +8,30 @@ namespace daxa
 {
     struct ImplDevice;
 
+    /// I (pahrens) am going to document the internals here as wsi is really confussng and strange in vulkan.
+    /// Every frame we get a swapcahin image index. This index can be non sequential in the case of mail box presentation and other modes.
+    /// This means we need to aquire a new index every frame to know what swapchain image to use.
+    ///
+    /// IMPORTANT INFORMATION REGUARDING SEMAPHORES IN WSI:
+    /// binary semaphore in aquire MUST be unsignaled when recording actions ON THE CPU!
+    ///
+    /// We need two binary semaphores here:
+    /// The acquire semaphore and the present semaphore.
+    /// The present semaphore is signaled in the last submission that uses the swapchain image and waited on in the present.
+    /// The acquire semaphore is signaled when the swapchain image is ready to be used.
+    /// This also means that the previous presentation of the image is finished and the semaphore used in the present is unsignaled.
+    /// Unformtunately there is NO other way to know when a present finishes (or the corresponding semaphore is unsignaled). 
+    /// This means that in order to be able to reuse binary semaphores used in presentation,
+    /// one MUST pair them with the image they are used to present.
+    /// 
+    /// One can then rely on the aquire semaphore of the image beeing signaled to indicate that the present semaphore is able to be reused,
+    /// As a swapchain images acquire sema is signaled when it is available and its previous present is completed.
+    /// 
+    /// In order to reuse the the acquire semaphore we must set a limit in frames in flight and wait on the cpu to limit the frames in flight.
+    /// When we have this wait in place we can savely reuse the acquire semaphores with a linearly increasing index corresponding to the frame.
+    /// This means the scquire semaphores are not tied to the number of swapchain images like present semaphores but to the number of frames in flight!!
+    ///
+    /// To limit the frames in flight we employ a timeline semaphore that must be signaled in a submission that uses or after one that uses the swpchain image. 
     struct ImplSwapchain final : ManagedSharedState
     {
         ManagedWeakPtr impl_device = {};
@@ -28,7 +52,6 @@ namespace daxa
         // This is the swapchain image index that acquire returns. THis is not nessecarily linear.
         // This index must be used for present semaphores as they are paired to the images.
         u32 current_image_index = {};
-        usize frames_in_flight = {};
 
         auto get_index_of_image(ImageId image) const -> usize;
 
