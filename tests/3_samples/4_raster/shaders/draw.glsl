5d7fdda8ee4163c477de1699fa67127e19bb3473
Patrick Ahrens <patrick.ahrens.32369@web.de>
2022-11-14 21:22:23 +0100

    refactored daxa.glsl to be more consistent and easier to use. Added some convenience makros for manual binding definitions.

--- tests/3_samples/4_raster/shaders/draw.glsl
+++ tests/3_samples/4_raster/shaders/draw.glsl
@@ -218,8 +218,8 @@ void main()
 {
     f32vec4 tex_col = texture(
         sampler2DArray(
-            daxa_access_Image(texture2DArray, push_constant.atlas_texture),
-            daxa_access_Sampler(sampler, push_constant.atlas_sampler)),
+            daxa_access_SampledImage(texture2DArray, push_constant.atlas_texture),
+            daxa_access_Sampler(push_constant.atlas_sampler)),
         v_tex_uv);
     f32vec3 col = tex_col.rgb;
     col *= max(dot(normalize(v_nrm), normalize(f32vec3(1, -3, 2))) * 0.5 + 0.5, 0.0);
