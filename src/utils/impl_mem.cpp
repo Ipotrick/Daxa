e0e760c39ea995b43b3fd267e5de05db465b6ff2
Patrick Ahrens <patrick.ahrens.32369@web.de>
2022-12-21 03:36:30 +0100

    Replaced staging memory allocator.

--- src/utils/impl_mem.cpp
+++ src/utils/impl_mem.cpp
@@ -4,79 +4,88 @@
 
 namespace daxa
 {
-    MemoryUploader::MemoryUploader(Device a_device, usize a_capacity)
-        : device{std::move(a_device)},
-          gpu_timeline{this->device.create_timeline_semaphore({.initial_value = 0, .debug_name = "MemoryUploader"})},
-          current_command_list{this->device.create_command_list({.debug_name = "MemoryUploader CommandList Nr. 0"})},
-          claimed_sizes{ClaimedSize{.timeline_value = 1, .size = 0}},
-          capacity{a_capacity}
+    TransferMemoryPool::TransferMemoryPool(TransferMemoryPoolInfo const & info)
+        : info{info},
+          gpu_timeline{this->info.device.create_timeline_semaphore({
+              .initial_value = {},
+              .debug_name = this->info.debug_name + ": timeline semaphore",
+          })},
+          buffer{this->info.device.create_buffer({
+              .size = this->info.capacity,
+              .memory_flags = daxa::MemoryFlagBits::HOST_ACCESS_RANDOM,
+              .debug_name = this->info.debug_name + ": buffer",
+          })}
     {
     }
 
-    auto MemoryUploader::upload_to_buffer(BufferId dst_buffer, usize dst_offset, usize size) -> void *
+    auto TransferMemoryPool::allocate(u32 allocation_size) -> std::optional<TransferMemoryPool::Allocation>
     {
-        usize const src_offset = this->reserve_memory(size);
-
-        this->claimed_sizes.back().size += size;
-
-        current_command_list.copy_buffer_to_buffer({
-            .src_buffer = upload_buffer,
-            .src_offset = src_offset,
-            .dst_buffer = dst_buffer,
-            .dst_offset = dst_offset,
-            .size = size,
-        });
-
-        return reinterpret_cast<void *>(this->device.get_host_address_as<u8>(this->upload_buffer) + src_offset);
-    }
-
-    auto MemoryUploader::get_commands() -> MemoryUploadCommandSubmitInfo
-    {
-        this->reclaim_unused_memory();
-
-        this->current_command_list.complete();
-
-        MemoryUploadCommandSubmitInfo ret{
-            .command_list = this->current_command_list,
-            .timeline = this->gpu_timeline,
-            .timeline_signal_value = this->timeline_value,
-        };
-
-        this->current_command_list = this->device.create_command_list({
-            .debug_name = std::string("MemoryUploader CommandList Nr. ") + std::to_string(timeline_value),
-        });
-
-        this->timeline_value += 1;
-
-        claimed_sizes.push_back({
-            .timeline_value = this->timeline_value,
-            .size = 0,
-        });
-
-        return ret;
-    }
-
-    void MemoryUploader::reclaim_unused_memory()
-    {
-        usize const gpu_timeline_value = gpu_timeline.value();
-        while (true)
+        // Firstly, test if there is enough continuous space left to allocate.
+        bool tail_allocation_possible =
+            (this->claimed_start + this->claimed_size + allocation_size) < this->info.capacity;
+        // When there is no tail space left, it may be the case that we can place the allocation at offset 0.
+        // Illustration: |XXX ## |; "X": new allocation; "#": used up space; " ": free space.
+        bool zero_offset_allocation_possible =
+            allocation_size < this->claimed_start &&
+            ((this->claimed_start + this->claimed_size) < this->info.capacity);
+        if (!tail_allocation_possible && !zero_offset_allocation_possible)
         {
-            if (claimed_sizes.front().timeline_value <= gpu_timeline_value)
+            this->reclaim_unused_memory();
+            tail_allocation_possible =
+                (this->claimed_start + this->claimed_size + allocation_size) < this->info.capacity;
+            zero_offset_allocation_possible =
+                allocation_size < this->claimed_start &&
+                ((this->claimed_start + this->claimed_size) < this->info.capacity);
+            if (!tail_allocation_possible && !zero_offset_allocation_possible)
             {
-                claimed_start = (claimed_start + claimed_sizes.front().size) % this->capacity;
-                claimed_sizes.pop_front();
+                return std::nullopt;
             }
+        }
+        current_timeline_value += 1;
+        u32 actual_allocation_size = {};
+        u32 allocation_offset = {};
+        if (tail_allocation_possible)
+        {
+            actual_allocation_size = allocation_size;
+            allocation_offset = (this->claimed_start + this->claimed_size) % this->info.capacity;
+        }
+        else // Zero offset allocation.
+        {
+            u32 left_tail_space = this->info.capacity - (this->claimed_start + this->claimed_size);
+            actual_allocation_size = allocation_size + left_tail_space;
+            allocation_offset = 0;
+        }
+        this->claimed_size += actual_allocation_size;
+        live_allocations.push_back(TrackedAllocation{
+            .timeline_index = this->current_timeline_value,
+            .size = actual_allocation_size,
+        });
+        return Allocation{
+            .device_address = this->buffer_device_address + allocation_offset,
+            .host_address = reinterpret_cast<void *>(this->buffer_host_address + allocation_offset),
+            .size = allocation_size,
+            .timeline_index = this->current_timeline_value,
+        };
+    }
+
+    auto TransferMemoryPool::timeline_value() const -> usize
+    {
+        return this->current_timeline_value;
+    }
+
+    void TransferMemoryPool::reclaim_unused_memory()
+    {
+        auto const current_gpu_timeline_value = this->gpu_timeline.value();
+        while (!live_allocations.empty() && live_allocations.front().timeline_index <= current_gpu_timeline_value)
+        {
+            this->claimed_start = (this->claimed_start + live_allocations.front().size) % this->info.capacity;
+            live_allocations.pop_front();
         }
     }
 
-    auto MemoryUploader::reserve_memory(usize size) -> usize
+    auto TransferMemoryPool::get_info() const -> TransferMemoryPoolInfo const &
     {
-        usize const start = this->claimed_start;
-        usize const old_size = this->claimed_size;
-        this->claimed_size += size;
-        DAXA_DBG_ASSERT_TRUE_M(this->claimed_size <= capacity, "exceeded MemoryUploader ring buffer capacity! A potential fix would be to increase the uploader's capacity.");
-
-        return (start + old_size) % capacity;
+        return this->info;
     }
 } // namespace daxa
 
