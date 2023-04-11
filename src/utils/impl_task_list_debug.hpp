52bfd532dccc2eb12d386af4ae5350aae4c96616
Patrick Ahrens <patrick.ahrens.32369@web.de>
2023-04-11 02:46:57 +0200

    Started to rework validation and validation messages for task list. Started to properly implement transient memory allocation and overlaping resources.
    Co-authored-by: Matěj Sakmary <matej.hydro@gmail.com>

--- src/utils/impl_task_list_debug.hpp
+++ src/utils/impl_task_list_debug.hpp
@@ -11,11 +11,57 @@ namespace daxa
         VERBOSE
     };
 
-    void print_task_image(std::ostream & os, ImplTaskList const & impl, TaskResourceIndex image, Level level)
+    struct FormatIndent
     {
-        // BASIC - name and usage
-        // DETAILED - above and mip levels, array layers, acesses
-        // VERBOSE - above and ids and layous
-        std::string ret = std::format("test {}\n", "3");
+        std::string & indent;
+        FormatIndent(std::string & a_indent) : indent{ a_indent } 
+        {
+            indent.push_back(' ');
+            indent.push_back(' ');
+            indent.push_back(' ');
+            indent.push_back(' ');
+        }
+        ~FormatIndent()
+        {
+            indent.pop_back();
+            indent.pop_back();
+            indent.pop_back();
+            indent.pop_back();
+        }
+    };
+
+    void print_task_image(std::string & out, std::string & indent, TaskListPermutation const & permutation, ImplTaskList const & impl, TaskResourceIndex image)
+    {
+        std::string_view const & name = impl.global_image_infos[image].get_name();
+        std::string persistent_info = "";
+        if (impl.global_image_infos[image].is_persistent())
+        {
+            u32 const persistent_index = impl.global_image_infos[image].get_persistent().unique_index;
+            persistent_info = std::format("persistent index: {}, ", persistent_index);
+        }
+        std::format_to(
+            std::back_inserter(out), 
+            "{}TaskImage: id: {}, name: {}, {}runtime images:\n", 
+            indent, 
+            to_string(TaskImageId{{.task_list_index = impl.unique_index, .index=image}}),
+            name,
+            persistent_info
+        );
+        TaskImageId const local_id = TaskImageId{{.task_list_index = impl.unique_index, .index = image }};
+        {
+            FormatIndent d0{indent};
+            for (u32 child_i = 0; child_i < impl.get_actual_images(local_id, permutation).size(); ++child_i)
+            {
+                auto const child_id = impl.get_actual_images(local_id, permutation)[child_i];
+                auto const & child_info = impl.info.device.info_image(child_id);
+                std::format_to(
+                    std::back_inserter(out), 
+                    "{}id: {}, name: {}\n",
+                    indent,
+                    to_string(child_id),
+                    child_info.name
+                );
+            }
+        }
     }
 }
