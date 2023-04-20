8a3cb596d1cd8f14e8543bfa769075c0fc403824
Patrick Ahrens <patrick.ahrens.32369@web.de>
2023-04-20 23:19:11 +0200

    Fixedboid sample code.

--- tests/3_samples/5_boids/shaders/update_boids.glsl
+++ tests/3_samples/5_boids/shaders/update_boids.glsl
@@ -3,118 +3,134 @@
 
 DAXA_USE_PUSH_CONSTANT(UpdateBoidsPushConstant, push)
 
-float good_sign(float v)
+void update_boid(inout Boid boid, in const Boid old_boid, in uint boid_index, BufferPtr(Boids) old_boids_buffer)
 {
-    return v > 0.0f ? 1.0f : -1.0f;
-}
+    boid = old_boid;
+    // Zero all accumulator variables
+    // xpos_avg, ypos_avg, xvel_avg, yvel_avg, neighboring_boids, close_dx, close_dy = 0
 
-float angle_between_normals(vec2 a, vec2 b)
-{
-    return acos(dot(a, b));
-}
-
-float angle_between_normals_sign(vec2 a, vec2 b)
-{
-    vec2 tangent_direction = vec2(-a.y, a.x);
-    return good_sign(dot(tangent_direction, b));
-}
-
-float signed_angle_between_normals(vec2 a, vec2 b)
-{
-    return angle_between_normals_sign(a, b) * angle_between_normals(a, b);
-}
-
-void boid_update_position(inout Boid boid, in Boid old_boid, vec2 new_boid_direction)
-{
-    vec2 new_position = old_boid.position + new_boid_direction * BOID_SPEED * SIMULATION_DELTA_TIME_S;
-
-    new_position = clamp(new_position, vec2(0.001f), vec2(FIELD_SIZE - 0.001f));
-
-    boid.position = new_position;
-    boid.direction = new_boid_direction;
-}
-
-void boid_avoid_walls(inout Boid old_boid, inout float steer_angle, inout float acc_steering_weight)
-{
-    vec2 wall_normal = vec2(0.0f);
-    float wall_dist = FIELD_SIZE;
-    // left wall
-    if (old_boid.position.x < BOID_VIEW_RANGE)
+    vec2 vis_other_pos_average = vec2(0,0);
+    vec2 vis_other_vel_average = vec2(0,0);
+    float vis_other_count = 0;
+    vec2 vis_other_super_close_delta = vec2(0,0);
+//
+    // # For every other boid in the flock . . .
+    // for each other boid (otherboid):
+    for (uint other_i = 0; other_i < MAX_BOIDS; ++other_i)
     {
-        wall_normal += vec2(1.0f, 0.0f);
-        wall_dist = min(wall_dist, old_boid.position.x);
-    }
-    // top wall
-    if (old_boid.position.y < BOID_VIEW_RANGE)
-    {
-        wall_normal += vec2(0.0f, 1.0f);
-        wall_dist = min(wall_dist, old_boid.position.y);
-    }
-    // right wall
-    if (old_boid.position.x + BOID_VIEW_RANGE > FIELD_SIZE)
-    {
-        wall_normal += vec2(-1.0f, 0.0f);
-        wall_dist = min(wall_dist, FIELD_SIZE - old_boid.position.x);
-    }
-    // bottom wall
-    if (old_boid.position.y + BOID_VIEW_RANGE > FIELD_SIZE)
-    {
-        wall_normal += vec2(0.0f, -1.0f);
-        wall_dist = min(wall_dist, FIELD_SIZE - old_boid.position.y);
-    }
-
-    if (wall_normal != vec2(0.0f))
-    {
-        wall_normal = normalize(wall_normal);
-        vec2 boid_to_wall_dir = -wall_normal;
-        float relative_closeness = 1.0f - (wall_dist / float(BOID_VIEW_RANGE)) * 0.5f;
-        float angle_to_move_boid_away = signed_angle_between_normals(old_boid.direction, boid_to_wall_dir);
-        angle_to_move_boid_away = clamp(angle_to_move_boid_away, -BOID_STEER_PER_TICK * 2, BOID_STEER_PER_TICK * 2);
-        float acc_weight = relative_closeness * 2;
-        steer_angle += angle_to_move_boid_away * relative_closeness * acc_weight;
-        acc_steering_weight += acc_weight;
-    }
-}
-
-void update_boid(inout Boid boid, in Boid old_boid, in uint boid_index, BufferPtr(Boids) old_boids_buffer)
-{
-    float acc_steer_angle = 0.0f;
-    float acc_steer_angle_weight = 0.0f;
-    boid_avoid_walls(old_boid, acc_steer_angle, acc_steer_angle_weight);
-
-    for (uint i = 0; i < 0; ++i)
-    {
-        if (i == boid_index)
+        if (other_i == boid_index)
+        {
             continue;
-
-        Boid other = deref(old_boids_buffer).boids[i];
-
-        float dst_to_other = length(other.position - old_boid.position);
-        float closeness_to_other = 1.0f - (dst_to_other / float(BOID_VIEW_RANGE));
-        vec2 dir_to_other = normalize(other.position - old_boid.position);
-        float signed_angle_of_boid_dir_to_other_dir = signed_angle_between_normals(old_boid.direction, dir_to_other);
-
-        if (dst_to_other < BOID_VIEW_RANGE || abs(signed_angle_of_boid_dir_to_other_dir) > BOID_VIEW_ANGLE)
-            continue;
-
-        // avoid other boids:
-        float avoid_acc_weight = closeness_to_other;
-        float avoid_angle_add = clamp(signed_angle_of_boid_dir_to_other_dir, -BOID_STEER_PER_TICK, BOID_STEER_PER_TICK) * BOIDS_AVOID_FACTOR;
-
-        // gabe take a look:
-        // acc_steer_angle += avoid_acc_weight * closeness_to_other * avoid_angle_add;
-        // acc_steer_angle_weight += avoid_acc_weight;
+        }
+        Boid other = deref(old_boids_buffer).boids[other_i];
+        // # Compute differences in x and y coordinates
+        // dx = boid.x - otherboid.x
+        // dy = boid.y - otherboid.y
+        const vec2 pos_delta = boid.position - other.position;
+        // # Are both those differences less than the visual range?
+        // if (abs(dx)<visual_range and abs(dy)<visual_range):  
+        // # Are both those differences less than the visual range?
+        // if (abs(dx)<visual_range and abs(dy)<visual_range):  
+        // # If so, calculate the squared distance
+        // squared_distance = dx*dx + dy*dy
+        const float squared_distance = dot(pos_delta,pos_delta);
+        // # Is squared distance less than the protected range?
+        // if (squared_distance < protected_range_squared):
+        if (squared_distance < BOID_PROTECTED_RANGE_SQUARED)
+        {
+            // # If so, calculate difference in x/y-coordinates to nearfield boid
+            // close_dx += boid.x - otherboid.x 
+            // close_dy += boid.y - otherboid.y
+            vis_other_super_close_delta += boid.position - other.position;
+        }
+        // # If not in protected range, is the boid in the visual range?
+        // else if (squared_distance < visual_range_squared):
+        else if (squared_distance < BOID_VIEW_RANGE_SQUARED)
+        {
+            // # Add other boid's x/y-coord and x/y vel to accumulator variables
+            // xpos_avg += otherboid.x 
+            // ypos_avg += otherboid.y 
+            // xvel_avg += otherboid.vx
+            // yvel_avg += otherboid.vy
+            vis_other_pos_average += other.position;
+            vis_other_vel_average += other.speed;
+            // # Increment number of boids within visual range
+            // neighboring_boids += 1 
+            vis_other_count += 1;
+        }
     }
-
-    vec2 new_boid_direction = boid.direction;
-    if (acc_steer_angle_weight > 0.0f)
+    // # If there were any boids in the visual range . . .            
+    // if (neighboring_boids > 0): 
+    if (vis_other_count > 0.0f)
     {
-        acc_steer_angle /= acc_steer_angle_weight;
-        new_boid_direction = vec2(
-            dot(old_boid.direction, vec2(cos(acc_steer_angle), sin(acc_steer_angle))),
-            dot(old_boid.direction, vec2(-sin(acc_steer_angle), cos(acc_steer_angle))));
+        //# Divide accumulator variables by number of boids in visual range
+        //xpos_avg = xpos_avg/neighboring_boids 
+        //ypos_avg = ypos_avg/neighboring_boids
+        //xvel_avg = xvel_avg/neighboring_boids
+        //yvel_avg = yvel_avg/neighboring_boids
+        vis_other_pos_average /= vis_other_count;
+        vis_other_vel_average /= vis_other_count;
+        //# Add the centering/matching contributions to velocity
+        //boid.vx = (boid.vx + 
+        //           (xpos_avg - boid.x)*centering_factor + 
+        //           (xvel_avg - boid.vx)*matching_factor)
+        //boid.vy = (boid.vy + 
+        //           (ypos_avg - boid.y)*centering_factor + 
+        //           (yvel_avg - boid.vy)*matching_factor)
+        boid.speed = boid.speed + (vis_other_pos_average - boid.position) * BOID_COHERENCE + (vis_other_vel_average - boid.speed) * BOID_ALIGNMENT;
     }
-    boid_update_position(boid, old_boid, new_boid_direction);
+    // # Add the avoidance contribution to velocity
+    // boid.vx = boid.vx + (close_dx*avoidfactor)
+    // boid.vy = boid.vy + (close_dy*avoidfactor)
+    boid.speed += BOID_SEPERATION * vis_other_super_close_delta;
+    //# If the boid is near an edge, make it turn by turnfactor
+    //if outside top margin:
+    //    boid.vy = boid.vy + turnfactor
+    //if outside right margin:
+    //    boid.vx = boid.vx - turnfactor
+    //if outside left margin:
+    //    boid.vx = boid.vx + turnfactor
+    //if outside bottom margin:
+    //    boid.vy = boid.vy - turnfactor
+    if (boid.position.x > FIELD_SIZE)
+    {
+        boid.speed.x -= BOID_WALL_REPULSION;
+    }
+    if (boid.position.x < 0)
+    {
+        boid.speed.x += BOID_WALL_REPULSION;
+    }
+    if (boid.position.y > FIELD_SIZE)
+    {
+        boid.speed.y -= BOID_WALL_REPULSION;
+    }
+    if (boid.position.y < 0)
+    {
+        boid.speed.y += BOID_WALL_REPULSION;
+    }
+    // # Calculate the boid's speed
+    // # Slow step! Lookup the "alpha max plus beta min" algorithm
+    const float speed = max(0.01, length(boid.speed));
+    //# Enforce min and max speeds
+    //if speed < minspeed:
+    //    boid.vx = (boid.vx/speed)*minspeed
+    //    boid.vy = (boid.vy/speed)*maxspeed
+    //if speed > maxspeed:
+    //    boid.vx = (boid.vx/speed)*maxspeed
+    //    boid.vy = (boid.vy/speed)*maxspeed
+    if (speed < BOID_MIN_SPEED)
+    {
+        boid.speed = boid.speed / speed * BOID_MIN_SPEED;
+    }
+    if (speed > BOID_MAX_SPEED)
+    {
+        boid.speed = boid.speed / speed * BOID_MAX_SPEED;
+    }
+
+    // # Update boid's position
+    // boid.x = boid.x + boid.vx
+    // boid.y = boid.y + boid.vy
+    boid.position = boid.position + boid.speed * DELTA_TIME;
 }
 
 layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
