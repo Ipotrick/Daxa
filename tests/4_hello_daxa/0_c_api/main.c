6aad0a4b28484f1ddfa5165d2148a722525a2732
Ipotrick <patrick.ahrens.32369@web.de>
2023-10-23 21:27:33 +0200

    expanded c sample

--- tests/4_hello_daxa/0_c_api/main.c
+++ tests/4_hello_daxa/0_c_api/main.c
@@ -14,8 +14,14 @@ void handle_daxa_result(daxa_Result res)
 int main(void)
 {
     daxa_Instance instance;
-    handle_daxa_result(daxa_create_instance(&(daxa_InstanceInfo){0}, &instance));
+    handle_daxa_result(daxa_create_instance(&DAXA_DEFAULT_INSTANCE_INFO, &instance));
 
+    daxa_Device device;
+    daxa_DeviceInfo dinfo = DAXA_DEFAULT_DEVICE_INFO;
+    dinfo.name = (daxa_StringView){.data = "name", .size = 4};
+    handle_daxa_result(daxa_instance_create_device(instance, &dinfo, &device));
+
+    daxa_dvc_dec_refcnt(device);
     daxa_instance_dec_refcnt(instance);
     return 0;
 }
